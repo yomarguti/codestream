af539a64de6803c4ccdc13b35b7812c7df56d946
akonwi <akonwi@codestream.com>
2017-12-21 18:10:29 -0500

    reload atom after changing server url

--- atom/lib/codestream.js
+++ atom/lib/codestream.js
@@ -133,9 +133,11 @@ module.exports = {
 					"codestream:wipe-cache": () => indexedDB.deleteDatabase("CodeStream"),
 					"codestream:point-to-dev": () => {
 						atom.config.set("codestream.url", "https://tca3.codestream.us:9443");
+						atom.reload();
 					},
 					"codestream:point-to-local": () => {
 						atom.config.set("codestream.url", "https://localhost.codestream.us:12079");
+						atom.reload();
 					}
 				})
 			);
