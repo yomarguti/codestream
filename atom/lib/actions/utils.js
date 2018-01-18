67a316ef5535d9ab2bc51de1c5fbd9739633c691
akonwi <akonwi@codestream.com>
2018-01-18 10:18:23 -0500

    refactoring

--- atom/lib/actions/utils.js
+++ atom/lib/actions/utils.js
@@ -1,4 +1,8 @@
-const normalizeObject = ({ _id, ...rest }) => ({ id: _id, ...rest });
+const normalizeObject = ({ _id, ...rest }) => {
+	const result = { ...rest };
+	if (_id) result.id = _id;
+	return result;
+};
 
 export const normalize = data => {
 	if (Array.isArray(data)) return data.map(normalizeObject);
