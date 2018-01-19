c20f95b705feb8afad44b56f5afd2d07fe59b3f5
akonwi <akonwi@codestream.com>
2018-01-19 09:48:43 -0500

    Limit scope of escape key handler so it doesn't intefere with the rest of the UX

--- atom/lib/components/Stream.js
+++ atom/lib/components/Stream.js
@@ -43,7 +43,7 @@ export class SimpleStream extends Component {
 			})
 		);
 		this.subscriptions.add(
-			atom.commands.add("atom-workspace", {
+			atom.commands.add(".codestream", {
 				"codestream:escape": event => this.handleEscape(event)
 			})
 		);
