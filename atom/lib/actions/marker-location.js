695b644ba523255e6fe867aeb8c2464cdb84279a
akonwi <akonwi@codestream.com>
2018-01-20 14:40:44 -0500

    refactor markerLocations storage

--- atom/lib/actions/marker-location.js
+++ atom/lib/actions/marker-location.js
@@ -2,8 +2,9 @@ import { upsert } from "../local-cache";
 
 export const saveMarkerLocations = attributes => (dispatch, getState, { db }) => {
 	return upsert(db, "markerLocations", attributes)
-		.then(locations => dispatch({ type: "ADD_MARKER_LOCATIONS", payload: locations }))
-		.catch("DataError", () => {
+		.then(record => dispatch({ type: "ADD_MARKER_LOCATIONS", payload: record }))
+		.catch(error => {
+			console.warn(error);
 			/* DataError is thrown when the primary key is not on the object.
 				 Assuming that means it's an empty object, we can swallow this since it's no-op */
 		});
--- atom/spec/actions/marker-location-spec.js
+++ atom/spec/actions/marker-location-spec.js
@@ -1,6 +1,8 @@
 import Dexie from "dexie";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
 import { dispatch } from "../redux-mocks";
-import { markerDirtied } from "../../lib/actions/marker-location";
+import { markerDirtied, saveMarkerLocations } from "../../lib/actions/marker-location";
 
 const dbName = "marker-location-spec";
 
@@ -11,12 +13,68 @@ describe("marker-location action creators", () => {
 	beforeEach(() => {
 		db = new Dexie(dbName);
 		db.version(1).stores({
-			markerLocations: "commitHash, streamId"
+			markerLocations: "[streamId+teamId+commitHash]"
 		});
 	});
 
 	afterEach(() => {
 		Dexie.delete(dbName);
+	});
+
+	describe("saveMarkerLocations", () => {
+		const teamId = "t1";
+		const streamId = "s1";
+		const markerId = "m1";
+		const markerId2 = "m2";
+		const commitHash = "c1";
+		const location = [7, 0, 13, 0];
+		const location2 = [0, 7, 3, 0];
+
+		it("saves locations by streamId+teamId+commitHash", () => {
+			const store = configureStore([thunk.withExtraArgument({ db })])();
+			const markerLocations = {
+				teamId,
+				streamId,
+				commitHash,
+				locations: { [markerId]: location, [markerId2]: location2 }
+			};
+
+			waitsForPromise(async () => {
+				await store.dispatch(saveMarkerLocations(markerLocations));
+				expect(store.getActions()).toContain({
+					type: "ADD_MARKER_LOCATIONS",
+					payload: markerLocations
+				});
+				const record = await db.markerLocations.get({ streamId, teamId, commitHash });
+				expect(record).toEqual(markerLocations);
+			});
+		});
+
+		it("updates an existing record for streamId+teamId+commitHash", () => {
+			const store = configureStore([thunk.withExtraArgument({ db })])();
+			const markerLocations = {
+				teamId,
+				streamId,
+				commitHash,
+				locations: { [markerId]: location2 }
+			};
+			waitsForPromise(async () => {
+				await db.markerLocations.add({
+					streamId,
+					teamId,
+					commitHash,
+					locations: { [markerId]: location }
+				});
+
+				await store.dispatch(saveMarkerLocations(markerLocations));
+				expect(store.getActions()).toContain({
+					type: "ADD_MARKER_LOCATIONS",
+					payload: markerLocations
+				});
+				const record = await db.markerLocations.get({ streamId, teamId, commitHash });
+				expect(record).toEqual(markerLocations);
+			});
+		});
 	});
 
 	describe("dirtied references due to buffer changes", () => {
