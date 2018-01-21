de2153707444d679769477e214bfc02428a8c710
akonwi <akonwi@codestream.com>
2018-01-21 12:02:05 -0500

    track marker movements when file is edited outside of atom

--- atom/lib/components/ReferenceBubble.js
+++ atom/lib/components/ReferenceBubble.js
@@ -34,21 +34,8 @@ class ReferenceBubble extends Component {
 			invalidate: "never"
 		});
 		this.subscriptions.add(
-			this.props.editor.onDidSave(event => {
-				console.group(`marker for reference ${this.props.id}`);
-				console.debug("saved location is", this.props.location);
-				const { start, end } = this.marker.getBufferRange();
-				const newLocation = Location(end, start);
-				console.debug("new location is", newLocation);
-				if (!_.isEqual(newLocation, this.props.location)) {
-					this.props.markerDirtied(
-						{ markerId: this.props.id, streamId: this.props.streamId },
-						newLocation
-					);
-					this.setState({ isVisible: isValid(newLocation) });
-				}
-				console.groupEnd();
-			}),
+			this.props.editor.getBuffer().onDidReload(this.onDidWriteToDisk),
+			this.props.editor.onDidSave(this.onDidWriteToDisk),
 			this.marker.onDidDestroy(() => {
 				this.subscriptions.dispose();
 			})
@@ -59,6 +46,22 @@ class ReferenceBubble extends Component {
 		this.marker.destroy();
 		this.subscriptions.dispose();
 	}
+
+	onDidWriteToDisk = () => {
+		console.group(`marker for reference ${this.props.id}`);
+		console.debug("saved location is", this.props.location);
+		const { start, end } = this.marker.getBufferRange();
+		const newLocation = Location(end, start);
+		console.debug("new location is", newLocation);
+		if (!_.isEqual(newLocation, this.props.location)) {
+			this.props.markerDirtied(
+				{ markerId: this.props.id, streamId: this.props.streamId },
+				newLocation
+			);
+			this.setState({ isVisible: isValid(newLocation) });
+		}
+		console.groupEnd();
+	};
 
 	render() {
 		if (!this.state.isVisible) return false;
