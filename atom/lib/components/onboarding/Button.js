5a256469b0cd87d1829ca96b2ced04fd22bb8ec2
akonwi <akonwi@codestream.com>
2018-02-06 15:13:50 -0800

    Disable submit button during onboarding while offline

--- atom/lib/components/onboarding/Button.js
+++ atom/lib/components/onboarding/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import createClassString from "classnames";
 
 Button.defaultProps = {
@@ -6,17 +7,18 @@ Button.defaultProps = {
 	disabled: false,
 	loading: false
 };
-
-export default function Button({ children, className, disabled, loading, ...extras }) {
+function Button({ children, className, disabled, isOffline, loading, ...extras }) {
 	return (
 		<button
 			className={createClassString("native-key-bindings btn inline-block-tight", className, {
 				"btn-primary": !loading
 			})}
-			disabled={loading || disabled}
+			disabled={isOffline || loading || disabled}
 			{...extras}
 		>
 			{loading ? <span className="loading loading-spinner-tiny inline-block" /> : children}
 		</button>
 	);
 }
+
+export default connect(state => ({ isOffline: state.connectivity.offline }))(Button);
