c41382bb56324b610b04d6e30d8732358626597a
akonwi <akonwi@codestream.com>
2018-02-07 23:23:48 -0800

    cleanup tooltip when resetting AddCommentPopup2

--- atom/lib/components/AddCommentPopup2.js
+++ atom/lib/components/AddCommentPopup2.js
@@ -69,6 +69,7 @@ export default class AddCommentPopup extends Component {
 		this.destroyMarker();
 		this.subscriptions.dispose();
 		this.subscriptions = new CompositeDisposable();
+		this.tooltip.dispose();
 	};
 
 	render() {
