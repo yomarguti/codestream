3e79c5fb8c4259c6d30cf20b82546a34df4510cb
akonwi <akonwi@codestream.com>
2018-02-12 11:15:24 -0500

    :lipstick:

--- atom/lib/umi-middleware.js
+++ atom/lib/umi-middleware.js
@@ -3,9 +3,6 @@ import { incrementUMI } from "./actions/umi";
 export default store => next => action => {
 	const { context } = store.getState();
 	if (action.type === "POSTS-UPDATE_FROM_PUBNUB") {
-		// If post is not in currently visible stream i.e `action.payload.streamId !== streams.byFile[context.currentFile].id`
-		// and action.payload.creatorId !== session.userId
-
 		store.dispatch(incrementUMI(action.payload));
 	}
 	return next(action);
