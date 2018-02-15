5c7de80aeffb92abb4a84f0780352e5a0c25554d
Marcelo Bukowski de Farias <marcelo@codestream.com>
2018-02-14 21:04:54 -0500

    Aggressive marker recalculation Recalculate locations considering uncommitted changes

--- atom/lib/reducers/marker-locations.js
+++ atom/lib/reducers/marker-locations.js
@@ -1,5 +1,3 @@
-import { toMapBy } from "./utils";
-
 const initialState = { byStream: {} };
 
 const addLocation = (state, payload) => {
@@ -29,7 +27,6 @@ export default (state = initialState, { type, payload }) => {
 			);
 		}
 		case "MARKERLOCATIONS-UPDATE_FROM_PUBNUB":
-		case "MARKER_DIRTIED":
 		case "ADD_MARKER_LOCATIONS":
 			return addLocation(state, payload);
 
