3caa4230b57ea6bea4da63f8a4346dc04681433b
Marcelo Bukowski de Farias <marcelo@codestream.com>
2018-03-01 10:18:43 -0500

    Remove redundant statement

--- atom/lib/reducers/markers.js
+++ atom/lib/reducers/markers.js
@@ -9,7 +9,6 @@ export default (state = initialState, { type, payload }) => {
 		case "ADD_MARKERS":
 			return { ...state, ...toMapBy("id", payload) };
 		case "MARKERS-UPDATE_FROM_PUBNUB":
-			return { ...state, [payload.id]: payload };
 		case "ADD_MARKER":
 			return { ...state, [payload.id]: payload };
 		default:
