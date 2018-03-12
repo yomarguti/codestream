825efdabc7746e6e914e202582ca32d9ea73ad1f
akonwi <akonwi@codestream.com>
2018-03-12 16:06:37 -0400

    COD-435 open slack integration url in browser

--- atom/lib/components/SlackInfo.js
+++ atom/lib/components/SlackInfo.js
@@ -1,10 +1,14 @@
+import { shell } from "electron";
 import React from "react";
 import { FormattedMessage } from "react-intl";
 import { connect } from "react-redux";
 import Button from "./onboarding/Button";
 import { cancelSlackInfo as cancel } from "../actions/context";
+import { getPath } from "../network-request";
 
-export default connect(null, { cancel })(props => (
+const mapStateToProps = ({ context }) => ({ teamId: context.currentTeamId });
+
+export default connect(mapStateToProps, { cancel })(props => (
 	<div id="slack-info">
 		<h2>
 			<FormattedMessage id="slackInfo.header" defaultMessage="Slack Integration" />
@@ -15,7 +19,14 @@ export default connect(null, { cancel })(props => (
 		<p>
 			<FormattedMessage id="slackInfo.p2" />
 		</p>
-		<Button>Add to Slack</Button>
+		<Button
+			onClick={e => {
+				shell.openExternal(getPath(`/no-auth/slack/addtoslack?codestream_team=${props.teamId}`));
+				props.cancel();
+			}}
+		>
+			Add to Slack
+		</Button>
 		<small className="centered">
 			<a onClick={e => props.cancel()}>Cancel</a>
 		</small>
