f437b012fc8750af7410f3aa3acdbf37b55008d1
akonwi <akonwi@codestream.com>
2018-04-02 13:09:08 -0400

    COD-477

--- atom/lib/components/InviteTeamMembersForm.js
+++ atom/lib/components/InviteTeamMembersForm.js
@@ -125,7 +125,7 @@ export class InvitePage extends Component {
 							</Tooltip>
 						</div>
 						<Button>
-							<FormattedMessage id="teamMemberSelection.add" defaultMessage="ADD" />
+							<FormattedMessage id="teamMemberSelection.add" defaultMessage="INVITE" />
 						</Button>
 					</div>
 				</form>
