d880183a357a9f07fdfa0c1d62954876b7d494e1
Eric Amodio <eric@codestream.com>
2018-04-17 14:02:58 -0400

    Add presence icon

--- vscode/src/views/userNode.ts
+++ vscode/src/views/userNode.ts
@@ -2,6 +2,7 @@
 import { TreeItem, TreeItemCollapsibleState } from 'vscode';
 import { CodeStreamSession, User } from '../api/session';
 import { ExplorerNode, ResourceType } from './explorerNode';
+import { Container } from '../container';
 
 export class UserNode extends ExplorerNode {
 
@@ -24,6 +25,7 @@ export class UserNode extends ExplorerNode {
         const item = new TreeItem(this.user.name, TreeItemCollapsibleState.None);
         item.id = this.id;
         item.contextValue = ResourceType.User;
+        item.iconPath = Container.context.asAbsolutePath(`assets/images/presence-online.svg`);
         return item;
     }
 }
