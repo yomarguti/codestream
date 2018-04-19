2d1c5de0d94827b21dd6066f20966bccd62041b1
Eric Amodio <eric@codestream.com>
2018-04-19 16:50:38 -0400

    Preserves webview if opening the same stream

--- vscode/src/container.ts
+++ vscode/src/container.ts
@@ -7,7 +7,7 @@ import { configuration } from './configuration';
 // import { UnreadDecorationProvider } from './providers/decorationProvider';
 import { CodeStreamExplorer } from './views/explorer';
 import { Git } from './git/git';
-import { StreamWebViewPanel } from './views/streamWebViewPanel';
+import { StreamWebViewController } from './views/streamWebViewPanel';
 import { CodeStreamCodeActionProvider } from './providers/codeActionProvider';
 import { CodeStreamCodeLensProvider } from './providers/codeLensProvider';
 import { CodeStreamMarkerDecorationProvider } from './providers/markerDecorationProvider';
@@ -31,7 +31,9 @@ export class Container {
         // context.subscriptions.push(this._codeLens = new CodeStreamCodeLensProvider());
         context.subscriptions.push(this._markerDecorations = new CodeStreamMarkerDecorationProvider());
         context.subscriptions.push(this._statusBar = new StatusBarController());
-        context.subscriptions.push(this._streamWebView = new StreamWebViewPanel(this._session));
+
+        context.subscriptions.push(this._streamWebView = new StreamWebViewController(this._session));
+        // context.subscriptions.push(this._streamWebView = new StreamWebViewPanel(this._session));
         // context.subscriptions.push(this._unreadDecorator = new UnreadDecorationProvider());
 
         if (config.explorer.enabled) {
@@ -106,7 +108,7 @@ export class Container {
         return this._session;
     }
 
-    private static _streamWebView: StreamWebViewPanel;
+    private static _streamWebView: StreamWebViewController;
     static get streamWebView() {
         return this._streamWebView;
     }
--- vscode/src/views/streamWebViewPanel.ts
+++ vscode/src/views/streamWebViewPanel.ts
@@ -1,119 +1,191 @@
-import {
-  Disposable,
-  ViewColumn,
-  Webview,
-  WebviewPanel,
-  window
-} from 'vscode';
+import { Disposable, Event, EventEmitter, ViewColumn, Webview, WebviewPanel, WebviewPanelOnDidChangeViewStateEvent, window } from 'vscode';
 import { CSPost, CSRepository, CSStream, CSTeam, CSUser } from '../api/api';
 import { CodeStreamSession, Stream } from '../api/session';
 import { Container } from '../container';
 import * as fs from 'fs';
+import { Logger } from '../logger';
 
 interface ViewData {
-  currentTeamId: string;
-  // currentRepoId: string;
-  currentUserId: string;
-  currentStreamId: string;
-  // currentFileId?: string;
-  // currentCommit?: string;
-  posts: CSPost[];
-  streams: CSStream[];
-  teams: CSTeam[];
-  users: CSUser[];
-  repos: CSRepository[];
+    currentTeamId: string;
+    // currentRepoId: string;
+    currentUserId: string;
+    currentStreamId: string;
+    // currentFileId?: string;
+    // currentCommit?: string;
+    posts: CSPost[];
+    streams: CSStream[];
+    teams: CSTeam[];
+    users: CSUser[];
+    repos: CSRepository[];
 }
 
 interface CSWebviewRequest {
-  type: string;
-  body: {
-    action: string;
-    params: any;
-  };
+    type: string;
+    body: {
+        action: string;
+        params: any;
+    };
 }
 
 class MessageRelay {
-  constructor(private readonly session: CodeStreamSession, private readonly view: Webview, private readonly stream: Stream) {
-    session.onDidReceivePosts(event => {
-      view.postMessage({
-        type: 'push-data',
-        body: {
-          type: 'posts',
-          payload: event.getPosts().map(post => (
-            { ...post.entity }
-          ))
-        }
-      });
-    });
+    constructor(
+        public readonly session: CodeStreamSession,
+        private readonly view: Webview,
+        private readonly stream: Stream
+    ) {
+        session.onDidReceivePosts(event => {
+            view.postMessage({
+                type: 'push-data',
+                body: {
+                    type: 'posts',
+                    payload: event.getPosts().map(post => ({ ...post.entity }))
+                }
+            });
+        });
 
-    view.onDidReceiveMessage(async (event: CSWebviewRequest) => {
-      const { type, body } = event;
-      if (type === 'action-request') {
-        if (body.action === 'post') {
-          const post = await this.stream.post(body.params.text);
-          post && this.view.postMessage({
-            type: 'action-response',
-            body: {
-              action: body.action,
-              payload: post.entity
+        view.onDidReceiveMessage(async (event: CSWebviewRequest) => {
+            const { type, body } = event;
+            if (type === 'action-request') {
+                if (body.action === 'post') {
+                    const post = await this.stream.post(body.params.text);
+                    post &&
+                        this.view.postMessage({
+                            type: 'action-response',
+                            body: {
+                                action: body.action,
+                                payload: post.entity
+                            }
+                        });
+                }
             }
-          });
-        }
-      }
-    }, null);
-  }
+        }, null);
+    }
 }
 
-export class StreamWebViewPanel implements Disposable {
+export class StreamWebViewController extends Disposable {
 
-  constructor(private session: CodeStreamSession) {
-  }
+    private _disposable: Disposable | undefined;
+    private _panel: StreamWebViewPanel | undefined;
 
-  dispose() {
-  }
+    constructor(public readonly session: CodeStreamSession) {
+        super(() => this.dispose());
+    }
 
-  async openStream(stream: Stream) {
-    const panel: WebviewPanel = window.createWebviewPanel(
-      'CodeStream.stream',
-      'CodeStream',
-      ViewColumn.Three,
-      {
-        retainContextWhenHidden: true,
-        enableFindWidget: true,
-        enableCommandUris: true,
-        enableScripts: true
-      }
-    );
+    dispose() {
+        this._disposable && this._disposable.dispose();
+        this._disposable = undefined;
+        this._panel = undefined;
+     }
 
-    const messageRelay = new MessageRelay(this.session, panel.webview, stream);
+    private onPanelClosed() {
+        this.dispose();
+    }
 
-    const state: ViewData = Object.create(null);
-    state.currentTeamId = stream.teamId;
-    state.currentUserId = this.session.userId;
-    state.currentStreamId = stream.id;
-    state.streams = [stream.entity];
+    async openStream(stream: Stream) {
+        if (this._panel === undefined) {
+            this._panel = new StreamWebViewPanel(this.session);
 
-    [
-      state.posts,
-      state.repos,
-      state.teams,
-      state.users
-    ] = await Promise.all([
-      stream.posts.entities,
-      this.session.repos.entities,
-      this.session.teams.entities,
-      this.session.users.entities
-    ]);
+            this._disposable = Disposable.from(
+                this._panel,
+                this._panel.onDidClose(this.onPanelClosed, this)
+            );
+        }
 
-    const htmlPath = Container.context.asAbsolutePath('/assets/index.html');
-    const scriptPath = Container.context.asAbsolutePath('/assets/app.js');
-    const stylesPath = Container.context.asAbsolutePath('/assets/styles/stream.css');
-    const html: string = fs.readFileSync(htmlPath, {
-      encoding: 'utf-8'
-    })
-    .replace('{% bootstrap-data %}', JSON.stringify(state))
-    .replace('{% script-path %}', scriptPath)
-    .replace('{% styles-path %}', stylesPath);
-    panel.webview.html = html;
-  }
+        this._panel.setStream(stream);
+    }
+}
+
+export class StreamWebViewPanel extends Disposable {
+
+    private _onDidClose = new EventEmitter<void>();
+    get onDidClose(): Event<void> {
+        return this._onDidClose.event;
+    }
+
+    private readonly _disposable: Disposable;
+    private readonly _panel: WebviewPanel;
+
+    constructor(private readonly session: CodeStreamSession) {
+        super(() => this.dispose());
+
+        this._panel = window.createWebviewPanel(
+            'CodeStream.stream',
+            'CodeStream',
+            ViewColumn.Three,
+            {
+                retainContextWhenHidden: true,
+                enableFindWidget: true,
+                enableCommandUris: true,
+                enableScripts: true
+            }
+        );
+
+        this._disposable = Disposable.from(
+            this._panel.onDidChangeViewState(this.onPanelViewStateChanged, this),
+            this._panel.onDidDispose(this.onPanelDisposed, this)
+        );
+    }
+
+    dispose() {
+        this._disposable && this._disposable.dispose();
+    }
+
+    is(stream: Stream) {
+        if (this._stream === undefined) return false;
+        return this._stream.id === stream.id;
+    }
+
+    show() {
+        this._panel.reveal(ViewColumn.Three);
+    }
+
+    private onPanelViewStateChanged(e: WebviewPanelOnDidChangeViewStateEvent) {
+        Logger.log('WebView.ViewStateChanged', e);
+    }
+
+    private onPanelDisposed() {
+        this._onDidClose.fire();
+    }
+
+    private _stream: Stream | undefined;
+    private _relay: MessageRelay | undefined;
+
+    async setStream(stream: Stream) {
+        if (this._stream && this._stream.id === stream.id) {
+            this.show();
+
+            return;
+        }
+
+        this._stream = stream;
+        this._relay = new MessageRelay(this.session, this._panel.webview, stream);
+
+        const state: ViewData = Object.create(null);
+        state.currentTeamId = stream.teamId;
+        state.currentUserId = this.session.userId;
+        state.currentStreamId = stream.id;
+        state.streams = [stream.entity];
+
+        [state.posts, state.repos, state.teams, state.users] = await Promise.all([
+            stream.posts.entities,
+            this.session.repos.entities,
+            this.session.teams.entities,
+            this.session.users.entities
+        ]);
+
+        const htmlPath = Container.context.asAbsolutePath('/assets/index.html');
+        const scriptPath = Container.context.asAbsolutePath('/assets/app.js');
+        const stylesPath = Container.context.asAbsolutePath('/assets/styles/stream.css');
+
+        const html = fs
+            .readFileSync(htmlPath, {
+                encoding: 'utf-8'
+            })
+            .replace('{% bootstrap-data %}', JSON.stringify(state))
+            .replace('{% script-path %}', scriptPath)
+            .replace('{% styles-path %}', stylesPath);
+        this._panel.webview.html = html;
+
+        this.show();
+    }
 }
