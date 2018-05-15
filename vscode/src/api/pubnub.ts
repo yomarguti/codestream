2e716a8a794653754fe26d6f1b43ad95667dff9f
Eric Amodio <eric@codestream.com>
2018-05-15 17:34:15 -0400

    Fixes pubnub subscription errors

--- vscode/src/api/pubnub.ts
+++ vscode/src/api/pubnub.ts
@@ -90,6 +90,14 @@ export class PubNubReceiver {
         });
     }
 
+    private unsubscribeCore(channels: string[]) {
+        if (channels.length === 0) return;
+
+        this._pubnub!.unsubscribe({
+            channels: channels
+        });
+    }
+
     private addListener() {
         this._listener = {
             presence: this.onPresence.bind(this),
@@ -128,6 +136,12 @@ export class PubNubReceiver {
         try {
             if ((status as any).error) {
                 Logger.warn(`PubNub.onStatus: ERROR event=${JSON.stringify(status)}`);
+
+                const errorStatus = (status as any) as PubNubErrorStatus;
+                if (errorStatus.category === Pubnub.CATEGORIES.PNAccessDeniedCategory) {
+                    const response = JSON.parse(errorStatus.errorData.response.text) as PubNubAccessDeniedErrorResponse;
+                    this.unsubscribeCore(response.payload.channels || []);
+                }
             }
             else {
                 Logger.log(`PubNub.onStatus: event=${JSON.stringify(status)}`);
@@ -168,35 +182,38 @@ export class PubNubReceiver {
         for (let [key, obj] of Object.entries(messages)) {
             Logger.log(`PubNub '${key}' message received\n${JSON.stringify(obj)}`);
 
-            if (Object.keys(obj).some(k => k.startsWith('$'))) {
-                Logger.log(`PubNub '${key}' message with directive received\n${JSON.stringify(obj)}`);
-                continue;
-            }
-
+            let entities;
             try {
                 switch (key) {
                     case 'post':
                     case 'repo':
                     case 'stream':
                         key += 's';
-                        obj = [obj];
+                        entities = [obj];
+                        break;
+                    default:
+                        entities = obj;
+                        break;
                 }
 
                 switch (key as MessageType) {
                     case 'posts':
-                        if (!obj || !obj.length) continue;
+                        entities = this.stripDirectives(key, entities);
+                        if (!entities || !entities.length) continue;
 
-                        this._onDidReceiveMessage.fire({ type: MessageType.Posts, posts: CodeStreamApi.normalizeResponse(obj) as CSPost[] });
+                        this._onDidReceiveMessage.fire({ type: MessageType.Posts, posts: CodeStreamApi.normalizeResponse(entities) as CSPost[] });
                         break;
                     case 'repos':
-                        if (!obj || !obj.length) continue;
+                        entities = this.stripDirectives(key, entities);
+                        if (!entities || !entities.length) continue;
 
-                        this._onDidReceiveMessage.fire({ type: MessageType.Repositories, repos: CodeStreamApi.normalizeResponse(obj) as CSRepository[] });
+                        this._onDidReceiveMessage.fire({ type: MessageType.Repositories, repos: CodeStreamApi.normalizeResponse(entities) as CSRepository[] });
                         break;
                     case 'streams':
-                        if (!obj || !obj.length) continue;
+                        entities = this.stripDirectives(key, entities);
+                        if (!entities || !entities.length) continue;
 
-                        const streams = CodeStreamApi.normalizeResponse(obj) as CSStream[];
+                        const streams = CodeStreamApi.normalizeResponse(entities) as CSStream[];
                         // Subscribe to any new non-file, non-team streams
                         this.subscribeCore([...Iterables.filterMap(streams, s => CodeStreamApi.isStreamSubscriptionRequired(s, this._userId!) ? `stream-${s.id}` : undefined)]);
 
@@ -208,203 +225,40 @@ export class PubNubReceiver {
                 Logger.error(ex, `PubNub '${key}' FAILED`);
             }
         }
-
-        // Raven.captureBreadcrumb({
-        //     message: "pubnub event",
-        //     category: "pubnub",
-        //     data: { requestId, isHistory, ...Object.keys(objects) },
-        //     level: "debug"
-        // });
-
-        // for (const [key, obj] of Object.entries(objects)) {
-        //     const handler = this.getMessageHandler(key);
-        //     if (handler) handler(obj, isHistory);
-        // }
-        // Object.keys(objects).forEach(key => {
-        //     const handler = this.getMessageHandler(key);
-        //     if (handler) handler(objects[key], isHistory);
-        // });
     }
 
-    // subscribe(channels) {
-    //     if (this.pubnub === null) throw new Error("PubNubReceiver must be initialized with an authKey and userId before subscribing to channels");
+    private stripDirectives(key: string, entities: any[] | undefined) {
+        if (!entities || !entities.length) return entities;
 
-    //     channels.forEach(channel => {
-    //         logger.debug("subscribing to", channel);
-    //         if (!this.subscriptions[channel]) {
-    //             this.subscriptions[channel] = new PubnubSubscription({
-    //                 pubnub: this.pubnub,
-    //                 channel: channel,
-    //                 store: this.store
-    //             });
-    //         }
-    //         this.subscriptions[channel].subscribe();
-    //     });
-    // }
+        return entities.filter(e => {
+            if (Object.keys(e).some(k => k.startsWith('$'))) {
+                Logger.log(`PubNub '${key}' message with directive skipped\n${JSON.stringify(e)}`);
+                return false;
+            }
+            return true;
+        });
+    }
+}
 
-    // unsubscribeAll() {
-    //     for (let channel in this.subscriptions) {
-    //         this.subscriptions[channel].unsubscribe();
-    //         delete this.subscriptions[channel];
-    //     }
-    //     this.removeListener();
-    // }
+interface PubNubErrorStatus {
+    error: boolean;
+    category: string; // Pubnub.Categories;
+    operation: Pubnub.Operations;
+    statusCode: number;
+    errorData: {
+        status: number;
+        response: {
+            text: string;
+        }
+    };
+}
 
-    // private getMessageHandler(type: any) {
-        // let tableName: string;
-        // switch (type) {
-        //     case 'stream':
-        //     case 'streams':
-        //         tableName = 'streams';
-        //         break;
-        //     case 'post':
-        //         return (data, isHistory) => this.store.dispatch(resolvePost(normalize(data), isHistory));
-        //     case 'posts':
-        //         tableName = 'posts';
-        //         break;
-        //     case 'user':
-        //     case 'users':
-        //         tableName = 'users';
-        //         break;
-        //     case 'team':
-        //     case 'teams':
-        //         tableName = 'teams';
-        //         break;
-        //     case 'repo':
-        //     case 'repos':
-        //         tableName = 'repos';
-        //         break;
-        //     case 'marker':
-        //     case 'markers':
-        //         tableName = 'markers';
-        //         break;
-        //     case 'markerLocations':
-        //         return (data, isHistory) =>
-        //             this.store.dispatch(saveMarkerLocations(normalize(data), isHistory));
-        // }
-        // if (tableName)
-        //     return (data, isHistory) =>
-        //         this.store.dispatch(resolveFromPubnub(tableName, normalize(data), isHistory));
-    // }
-
-    // getSubscribedChannels() {
-    //     return Object.keys(this.subscriptions).filter(channel =>
-    //         this.subscriptions[channel].isSubscribed()
-    //     );
-    // }
-
-    // retrieveHistory(channels, messaging = {}) {
-    //     let retrieveSince;
-    //     channels = channels || this.getSubscribedChannels();
-    //     if (messaging.lastMessageReceived) {
-    //         retrieveSince = messaging.lastMessageReceived;
-    //         // FIXME: there probably needs to be a time limit here, where we assume it isn't
-    //         // worth replaying all the messages ... instead we just wipe the DB and refresh
-    //         // the session ... maybe a week?
-    //         return this.retrieveHistorySince(channels, retrieveSince);
-    //     } else {
-    //         // assuming there's nothing cached yet and this is a clean slate
-    //         return this.store
-    //             .dispatch(fetchStreamsAndAllPosts())
-    //             .then(() => this.store.dispatch(caughtUp()));
-    //     }
-    // }
-
-    // async retrieveHistorySince(channels, timeToken) {
-    //     // fetch the history for each subscribed channel individually...
-    //     let allMessages = [];
-    //     await Promise.all(
-    //         channels.map(channel => {
-    //             return this.retrieveChannelHistorySince(channel, timeToken, allMessages);
-    //         })
-    //     );
-
-    //     // now get numeric timestamps (from the stringified time tokens) and sort based on
-    //     // timestamp ... to ensure we process messages in order
-    //     allMessages.forEach(message => {
-    //         message.timestamp = parseInt(message.timetoken, 10) / 10000;
-    //     });
-    //     allMessages.sort((a, b) => {
-    //         return a.timestamp - b.timestamp;
-    //     });
-
-    //     if (allMessages.length > 0) {
-    //         // store the last message received, so we know where to start from next time
-    //         const lastMessage = allMessages[allMessages.length - 1];
-    //         this.store.dispatch(lastMessageReceived(lastMessage.timetoken));
-    //     } else {
-    //         this.store.dispatch(caughtUp());
-    //     }
-
-    //     for (var message of allMessages) {
-    //         this.processMessage(message.entry, { isHistory: true });
-    //     }
-    //     return allMessages.length;
-    // }
-
-    // async retrieveChannelHistorySince(channel, timeToken, allMessages) {
-    //     let response = null;
-    //     let retries = 0;
-    //     let delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-    //     let delayTime = 1000;
-    //     while (!response) {
-    //         try {
-    //             Raven.captureBreadcrumb({
-    //                 message: 'retrieve history',
-    //                 category: 'pubnub',
-    //                 data: { timeToken },
-    //                 level: 'debug'
-    //             });
-    //             response = await this.pubnub.history({
-    //                 channel: channel,
-    //                 reverse: true, // oldest message first
-    //                 start: timeToken,
-    //                 stringifiedTimeToken: true
-    //             });
-    //         } catch (error) {
-    //             const now = new Date().toString();
-    //             console.warn(`${now}: PubNub history failed for ${channel}:`, error);
-    //             Raven.captureBreadcrumb({
-    //                 message: `retrieve history failed`,
-    //                 category: 'pubnub',
-    //                 data: { error },
-    //                 level: 'debug'
-    //             });
-    //             if (!navigator.onLine) {
-    //                 // here we give up prematurely, we'll wait until the signal that we are
-    //                 // online again to try
-    //                 console.warn(`${now}: HISTORY RETRIEVAL FAILED BUT WE ARE OFFLINE`);
-    //                 pollTillOnline();
-    //                 return true;
-    //             }
-    //             if (retries === 30) {
-    //                 // increase throttle time, till we reach one minute, then give up
-    //                 delayTime = 5000;
-    //             } else if (retries === 36) {
-    //                 console.warn(`${now}: Giving up fetching history for ${channel}`);
-    //                 Raven.captureBreadcrumb({
-    //                     message: `gave up fetching history`,
-    //                     category: 'pubnub',
-    //                     level: 'warning'
-    //                 });
-    //                 Raven.captureMessage(`history retrieval failure`);
-    //                 this.store.dispatch(historyRetrievalFailure());
-    //                 return true;
-    //             }
-    //             retries++;
-    //             await delay(delayTime);
-    //         }
-    //     }
-
-    //     // history was successfully retrieved for all channels
-    //     allMessages.push(...response.messages);
-    //     if (response.messages.length < 100) {
-    //         return true; // resolves the promise
-    //     } else {
-    //         // FIXME: we can't let this go on too deep, there needs to be a limit
-    //         // once we reach that limit, we probably need to just clear the database and
-    //         // refresh the session (like you're coming back from vacation)
-    //         return this.retrieveChannelHistorySince(channel, response.endTimeToken, allMessages);
-    //     }
-    // }
+interface PubNubAccessDeniedErrorResponse {
+    message: string;
+    payload: {
+        channels: string[];
+        error: boolean;
+        service: string;
+        status: number;
+    };
 }
