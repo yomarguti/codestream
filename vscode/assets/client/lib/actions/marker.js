bc4ebee80e3f72cbb07d5660e6df99170378d6cb
akonwi <akonwi@codestream.com>
2018-05-31 10:29:55 -0500

    :fire: delete unused files in assets/client

--- vscode/assets/client/lib/actions/marker.js
+++
@@ -1,9 +0,0 @@
-import { upsert } from "../local-cache";
-
-export const saveMarker = attributes => (dispatch, getState, { db }) => {
-	return upsert(db, "markers", attributes).then(marker =>
-		dispatch({ type: "ADD_MARKER", payload: marker })
-	);
-};
-
-export const saveMarkers = markers => ({ type: "ADD_MARKERS", payload: markers });
