82f08dfc1dd5b9c9032e92c362b53ca652609b29
akonwi <akonwi@codestream.com>
2018-06-04 02:09:35 -0500

    refactor rollup config

--- shared/ui/rollup.config.cjs.js
+++
@@ -1,8 +0,0 @@
-import config from "./rollup.config.es.js";
-
-export default Object.apply(config, {
-	output: {
-		file: "dist/codestream-components.cjs.js",
-		format: "cjs"
-	}
-});
--- shared/ui/rollup.config.js
+++ shared/ui/rollup.config.js
@@ -2,49 +2,42 @@ import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 
-const input = "index.js";
-const plugins = [
-	resolve({
-		module: true,
-		jsnext: true,
-		main: true,
-		extensions: [".js", ".json"],
-		preferBuiltins: false
-	}),
-	commonjs({
-		include: "node_modules/**",
-		extensions: [".js", ".json"],
-		namedExports: {
-			"node_modules/react/index.js": [
-				"Component",
-				"Children",
-				"createElement",
-				"Fragment",
-				"isValidElement",
-				"PureComponent"
-			]
-		}
-	}),
-	babel({
-		exclude: "node_modules/**"
-	})
-];
-
-export default [
-	{
-		input,
-		output: {
+export default {
+	input: "index.js",
+	output: [
+		{
 			file: "dist/codestream-components.es.js",
 			format: "es"
 		},
-		plugins
-	},
-	{
-		input,
-		output: {
+		{
 			file: "dist/codestream-components.cjs.js",
 			format: "cjs"
-		},
-		plugins
-	}
-];
+		}
+	],
+	plugins: [
+		resolve({
+			module: true,
+			jsnext: true,
+			main: true,
+			extensions: [".js", ".json"],
+			preferBuiltins: false
+		}),
+		commonjs({
+			include: "node_modules/**",
+			extensions: [".js", ".json"],
+			namedExports: {
+				"node_modules/react/index.js": [
+					"Component",
+					"Children",
+					"createElement",
+					"Fragment",
+					"isValidElement",
+					"PureComponent"
+				]
+			}
+		}),
+		babel({
+			exclude: "node_modules/**"
+		})
+	]
+};
