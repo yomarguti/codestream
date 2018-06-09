5c22fab7cfadbe1ede43e4e69da2ef6bdb9810b8
Peter Pezaris <pez@codestream.com>
2018-06-09 17:10:35 -0400

    oops forgot a file

---
+++ shared/ui/Stream/SlashCommands.js
@@ -0,0 +1,15 @@
+export const slashCommands = [
+	{ id: "help", help: "get help" },
+	{ id: "add", help: "add member to channel", description: "[@user]" },
+	{ id: "invite", help: "add to your team", description: "[email]" },
+	{ id: "leave", help: "leave channel" },
+	{ id: "me", help: "emote" },
+	{ id: "msg", help: "message member", description: "[@user]" },
+	{ id: "mute", help: "mute channel" },
+	// { id: "muteall", help: "mute codestream" },
+	// { id: "open", help: "open channel" },
+	// { id: "prefs", help: "open preferences" },
+	{ id: "rename", help: "rename channel", description: "[newname]" },
+	{ id: "remove", help: "remove from channel", description: "[@user]" },
+	{ id: "who", help: "show channel members" }
+];
