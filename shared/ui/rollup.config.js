7d8c0488c95d4f496215b9d04ecb3a4a05ad75f1
akonwi <akonwi@codestream.com>
2018-06-11 10:22:49 -0400

    add rollup-plugin-json to convert json files to modules

--- shared/ui/rollup.config.js
+++ shared/ui/rollup.config.js
@@ -1,6 +1,7 @@
 import babel from "rollup-plugin-babel";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import json from "rollup-plugin-json";
 
 export default {
 	input: "index.js",
@@ -15,6 +16,10 @@ export default {
 		}
 	],
 	plugins: [
+		json({
+			include: "node_modules/**",
+			preferConst: true
+		}),
 		resolve({
 			module: true,
 			jsnext: true,
