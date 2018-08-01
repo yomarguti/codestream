66f5e4cc07eee37dd3b0178902a548b123a7b92e
Eric Amodio <eric@codestream.com>
2018-08-01 17:47:33 -0400

    Fixes some build issues
    npm run rebuild will now handle everything

--- vscode/webpack.config.js
+++ vscode/webpack.config.js
@@ -2,14 +2,28 @@
 const path = require("path");
 const nodeExternals = require("webpack-node-externals");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
+const FileManagerWebpackPlugin = require("filemanager-webpack-plugin");
 
 module.exports = function(env, argv) {
 	env = env || {};
-
 	const production = !!env.production;
-	console.log("Production:", production);
 
-	const plugins = [new CleanWebpackPlugin(["dist"])];
+	const plugins = [
+		new CleanWebpackPlugin(["dist"]),
+		new FileManagerWebpackPlugin({
+			onEnd: [
+				{
+					copy: [
+						{
+							source: path.resolve(__dirname, "../codestream-lsp-agent/dist/*"),
+							// TODO: Use environment variable if exists
+							destination: "dist/"
+						}
+					]
+				}
+			]
+		})
+	];
 
 	return {
 		entry: "./src/extension.ts",
--- vscode/webview.webpack.config.js
+++ vscode/webview.webpack.config.js
@@ -63,7 +63,8 @@ module.exports = function(env, argv) {
 			modules: [path.resolve(__dirname, "src/webviews/app"), "node_modules"],
 			alias: {
 				// TODO: Use environment variable if exists
-				"codestream-components$": path.resolve(__dirname, "../codestream-components/index.js")
+				"codestream-components$": path.resolve(__dirname, "../codestream-components/index.js"),
+				"codestream-components": path.resolve(__dirname, "../codestream-components/")
 			}
 		},
 		module: {
