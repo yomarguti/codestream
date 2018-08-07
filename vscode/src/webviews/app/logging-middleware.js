173d67447dbe0c2d9549fbf783ca99e0a4f18856
akonwi <akonwi@codestream.com>
2018-08-07 18:20:51 -0400

    clean up logger for redux

--- vscode/src/webviews/app/logging-middleware.js
+++ vscode/src/webviews/app/logging-middleware.js
@@ -1,11 +1,14 @@
 export default store => {
 	return next => action => {
+		const oldState = Object.assign({}, store.getState());
+		const result = next(action);
+
 		console.groupCollapsed(action.type);
-		console.debug(action)
-		console.debug('current state', store.getState())
-		const result  = next(action)
-		console.debug('new state', store.getState())
-		console.groupEnd()
-		return result
+		console.debug(action);
+		console.debug("old state", oldState);
+		console.debug("new state", Object.assign({}, store.getState()));
+		console.groupEnd();
+
+		return result;
 	};
 };
