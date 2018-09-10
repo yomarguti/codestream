e5c3238a7047c5d74f55f0f0ca02351174a8fe03
akonwi <akonwi@codestream.com>
2018-09-10 16:59:42 -0400

    bootstrap properly when validating sign up

--- shared/ui/CompleteSignup/actions.js
+++ shared/ui/CompleteSignup/actions.js
@@ -1,12 +1,12 @@
+import { bootstrap } from "../actions";
 import { setContext } from "../actions/context";
 export { startSignup } from "../Login/actions";
 export { goToLogin } from "../actions/routing";
 
 export const validateSignup = () => async (dispatch, getState, { api }) => {
 	const response = await api.validateSignup();
-	dispatch({ type: "ADD_STREAMS", payload: response.streams });
-	dispatch({ type: "ADD_TEAMS", payload: response.teams });
-	dispatch({ type: "ADD_USERS", payload: response.users });
+	dispatch(bootstrap(response));
 	dispatch(setContext({ currentTeamId: response.currentTeamId }));
+	dispatch({ type: "UPDATE_UNREADS", payload: response.unreads });
 	dispatch({ type: "INIT_SESSION", payload: { userId: response.currentUserId } });
 };
