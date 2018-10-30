6d2790c5e38372d634744e2302a801c14234fdc3
Eric Amodio <eamodio@gmail.com>
2018-10-30 02:42:05 -0400

    Removes spinner text and fixes spinner background https://trello.com/c/1gtkghv3

--- shared/ui/Stream/infiniteLoadable.js
+++ shared/ui/Stream/infiniteLoadable.js
@@ -158,7 +158,6 @@ export default Child => {
 								<div className="loader-ring__segment" />
 								<div className="loader-ring__segment" />
 							</div>
-							<p>Loading posts...</p>
 						</div>
 					);
 
