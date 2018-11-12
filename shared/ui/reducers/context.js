3b776fdf15711c502fb4ab44f2862eafe4ed6e01
akonwi <akonwi@gmail.com>
2018-11-12 14:07:31 -0500

    :fire:

--- shared/ui/reducers/context.js
+++ shared/ui/reducers/context.js
@@ -5,10 +5,7 @@ const initialState = {
 	currentRepoId: "",
 	currentCommit: "",
 	panelStack: ["channels"], // stores the stack of panels
-	noAccess: false, // Legacy
-	showSlackInfo: false, // Legacy
-	hasFocus: true, // we assume we start with the focus when codestream initializes
-	usernamesInTeam: [] // Legacy
+	hasFocus: true // we assume we start with the focus when codestream initializes
 };
 
 export default (state = initialState, { type, payload }) => {
@@ -22,8 +19,6 @@ export default (state = initialState, { type, payload }) => {
 	}
 	if (type === "SET_CURRENT_TEAM") return { ...state, currentTeamId: payload };
 	if (type === "SET_CURRENT_STREAM") return { ...state, currentStreamId: payload };
-	if (type === "SET_CURRENT_REPO") return { ...state, currentRepoId: payload };
-	if (type === "SET_CURRENT_COMMIT") return { ...state, currentCommit: payload };
 	if (type === "SET_PANEL") {
 		// set the current panel and keep a stack of the most
 		// recent ones, so we can pop the current panel off the
@@ -37,10 +32,5 @@ export default (state = initialState, { type, payload }) => {
 	}
 	if (type === "COMMIT_HASH_CHANGED") return { ...state, currentCommit: payload };
 	if (type === "SET_HAS_FOCUS") return { ...state, hasFocus: payload };
-	if (type === "NO_ACCESS") return { ...state, noAccess: { noAccess: true } };
-	if (type === "NO_ACCESS-MISSING_REMOTE_URL") return { ...state, noAccess: { noUrl: true } };
-	if (type === "NO_GIT_IN_PATH") return { ...state, noAccess: { noGit: true } };
-	if (type === "SHOW_SLACK_INFO") return { ...state, showSlackInfo: true };
-	if (type === "CANCEL_SLACK_INFO") return { ...state, showSlackInfo: false };
 	return { ...initialState, ...state };
 };
