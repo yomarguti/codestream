985928eda54f3c26ea3765ea27d0733c2ef59cf7
akonwi <akonwi@codestream.com>
2018-11-20 03:30:06 -0500

    :nail_care:

--- shared/ui/reducers/context.ts
+++ shared/ui/reducers/context.ts
@@ -13,30 +13,39 @@ const initialState = {
 };
 
 export default (state = initialState, { type, payload }) => {
-	if (type === "RESET_CONTEXT")
-		return { ...initialState, currentFile: state.currentFile, currentCommit: state.currentCommit };
-	if (type === "SET_CONTEXT") return { ...state, ...payload };
-	if (type === "SET_CURRENT_FILE") {
-		const file = payload.editor && payload.editor.fileName;
-		if (file) return { ...state, currentFile: file, mostRecentSourceFile: file };
-		else return { ...state, currentFile: "" };
+	switch (type) {
+		case "RESET_CONTEXT":
+			return {
+				...initialState,
+				currentFile: state.currentFile,
+				currentCommit: state.currentCommit
+			};
+		case "SET_CONTEXT":
+			return { ...state, ...payload };
+		case "SET_CURRENT_FILE": {
+			const file = payload.editor && payload.editor.fileName;
+			if (file) return { ...state, currentFile: file, mostRecentSourceFile: file };
+			else return { ...state, currentFile: "" };
+		}
+		case "SET_CURRENT_TEAM":
+			return { ...state, currentTeamId: payload };
+		case "SET_CURRENT_STREAM":
+			return { ...state, currentStreamId: payload, threadId: null };
+		case Type.SetThread: {
+			return { ...state, currentStreamId: payload.streamId, threadId: payload.threadId };
+		}
+		case "SET_PANEL":
+			return { ...state, panelStack: [payload, ...state.panelStack].slice(0, 10) };
+		case "CLOSE_PANEL": {
+			if (state.panelStack.length === 1) return state;
+			const [, ...panelStack] = state.panelStack;
+			return { ...state, panelStack };
+		}
+		case "COMMIT_HASH_CHANGED":
+			return { ...state, currentCommit: payload };
+		case "SET_HAS_FOCUS":
+			return { ...state, hasFocus: payload };
+		default:
+			return { ...initialState, ...state };
 	}
-	if (type === "SET_CURRENT_TEAM") return { ...state, currentTeamId: payload };
-	if (type === "SET_CURRENT_STREAM") return { ...state, currentStreamId: payload, threadId: null };
-	if (type === Type.SetThread)
-		return { ...state, currentStreamId: payload.streamId, threadId: payload.threadId };
-	if (type === "SET_PANEL") {
-		// set the current panel and keep a stack of the most
-		// recent ones, so we can pop the current panel off the
-		// stack, and return to the prior one
-		return { ...state, panelStack: [payload, ...state.panelStack].slice(0, 10) };
-	}
-	if (type === "CLOSE_PANEL") {
-		if (state.panelStack.length === 1) return state;
-		const [, ...panelStack] = state.panelStack;
-		return { ...state, panelStack };
-	}
-	if (type === "COMMIT_HASH_CHANGED") return { ...state, currentCommit: payload };
-	if (type === "SET_HAS_FOCUS") return { ...state, hasFocus: payload };
-	return { ...initialState, ...state };
 };
