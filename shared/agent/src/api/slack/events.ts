yomarguti/codestream

commit 11b63c6fe68353870431bc5c0c633914136a5612

Fix cache resolver

shared/agent/src/managers/baseManager.ts
@@ -5,13 +5,35 @@ import { debug, log } from "../system";
 import { IndexParams } from "./cache";
 import { BaseCache, KeyValue } from "./cache/baseCache";
 import * as operations from "./operations";
-import { isCompleteObject } from "./operations";
+
+function isDirective(data: any): boolean {
+	return !!data.$version;
+}
+
+function isCompatibleVersion(cachedEntity: any, newEntityOrDirective: any): boolean {
+	if (isDirective(newEntityOrDirective)) {
+		const directiveVersion = newEntityOrDirective.version;
+		if (!directiveVersion) {
+			throw new Error(
+				`Received directive without version attribute for object Id=${newEntityOrDirective.id}`
+			);
+		}
+		return directiveVersion.before === cachedEntity.version;
+	} else {
+		if (cachedEntity.version == null && newEntityOrDirective.version == null) {
+			return true;
+		}
+		return cachedEntity.version < newEntityOrDirective.version;
+	}
+}
 
 export abstract class ManagerBase<T> {
 	protected readonly cache: BaseCache<T> = new BaseCache<T>({
 		idxFields: this.getIndexedFields(),
 		entityName: this.getEntityName()
 	});
+
+	protected forceFetchToResolveOnCacheMiss = false;
 
 	public constructor(public readonly session: CodeStreamSession) {
 		this.session.onDidRequestReset(() => {
@@ -44,13 +66,13 @@ export abstract class ManagerBase<T> {
 			message.data.map(async (data: any) => {
 				const criteria = this.fetchCriteria(data as T);
 				const cached = await this.cacheGet(criteria);
-				if (cached) {
+				if (cached && isCompatibleVersion(cached, data)) {
 					const updatedEntity = operations.resolve(cached as any, data);
 					this.cacheSet(updatedEntity as T, cached);
 					return updatedEntity as T;
 				} else {
 					let entity;
-					if (this.mustFetchToResolve(data)) {
+					if (this.forceFetchToResolveOnCacheMiss || isDirective(data)) {
 						entity = await this.fetch(criteria);
 					} else {
 						entity = data as T;
@@ -64,10 +86,6 @@ export abstract class ManagerBase<T> {
 			})
 		);
 		return resolved.filter(Boolean) as T[];
-	}
-
-	protected mustFetchToResolve(obj: object): boolean {
-		return isCompleteObject(obj);
 	}
 
 	cacheGet(criteria: KeyValue<T>[]): Promise<T | undefined> {

shared/agent/src/logger.ts
@@ -83,13 +83,11 @@ export class Logger {
 			}
 		}
 
-		if (message === undefined) {
-			const stack = ex.stack;
-			if (stack) {
-				const match = /.*\s*?at\s(.+?)\s/.exec(stack);
-				if (match != null) {
-					message = match[1];
-				}
+		const stack = ex.stack;
+		if (message === undefined && stack) {
+			const match = /.*\s*?at\s(.+?)\s/.exec(stack);
+			if (match != null) {
+				message = match[1];
 			}
 		}
 
@@ -99,7 +97,7 @@ export class Logger {
 
 		if (this._agent !== undefined) {
 			this._agent.error(
-				`${this.timestamp} ${message || ""}${this.toLoggableParams(false, params)}\n${ex}`
+				`${this.timestamp} ${message || ""}${this.toLoggableParams(false, params)}\n${ex}\n${stack}`
 			);
 		}
 

shared/agent/src/managers/markerLocationManager.ts
@@ -59,6 +59,8 @@ function newGetLocationsResult(): GetLocationsResult {
 }
 
 export class MarkerLocationManager extends ManagerBase<CSMarkerLocations> {
+	protected forceFetchToResolveOnCacheMiss = true;
+
 	getIndexedFields(): IndexParams<CSMarkerLocations>[] {
 		return [
 			{
@@ -77,10 +79,6 @@ export class MarkerLocationManager extends ManagerBase<CSMarkerLocations> {
 			};
 		}
 		super.cacheSet(entity, oldEntity);
-	}
-
-	protected mustFetchToResolve(obj: object): boolean {
-		return true;
 	}
 
 	protected async fetch(criteria: KeyValue<CSMarkerLocations>[]): Promise<CSMarkerLocations> {

shared/agent/src/managers/operations.ts
@@ -95,5 +95,8 @@ const operations = {
 				else if (Number.isInteger(value)) object[property] = value + data[property];
 			});
 		});
+	},
+	$version(object: any, data: any) {
+		object.version = data.after;
 	}
 } as any;

commit 1520fc894da0dac11dc329433fdd18fad5a1b1bc

Fixes issue with slack events allowing directives into the cache
Caused by commit 11b63c6

shared/agent/src/api/slack/events.ts
@@ -362,7 +362,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel,
-								$set: { isArchived: true }
+								$set: { isArchived: true },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -378,7 +379,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel,
-								$set: { isClosed: true }
+								$set: { isClosed: true },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -409,7 +411,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel,
-								$set: { deactivated: true }
+								$set: { deactivated: true },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -425,7 +428,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel,
-								$set: { isClosed: false }
+								$set: { isClosed: false },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -446,7 +450,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel,
-								$addToSet: { memberIds: [e.user] }
+								$addToSet: { memberIds: [e.user] },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -461,7 +466,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel,
-								$pull: { memberIds: [e.user] }
+								$pull: { memberIds: [e.user] },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -477,7 +483,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel,
-								$pull: { memberIds: [this._api.userId] }
+								$pull: { memberIds: [this._api.userId] },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -504,7 +511,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel.id,
-								$set: { name: e.channel.name }
+								$set: { name: e.channel.name },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -520,7 +528,8 @@ export class SlackEvents {
 						data: [
 							{
 								id: e.channel,
-								$set: { isArchived: false }
+								$set: { isArchived: false },
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as StreamsRTMessage;
@@ -581,7 +590,8 @@ export class SlackEvents {
 								data: [
 									{
 										id: e.channel,
-										$set: { purpose: e.purpose }
+										$set: { purpose: e.purpose },
+										$version: { before: "*" }
 									} as unknown
 								]
 							} as StreamsRTMessage;
@@ -656,7 +666,8 @@ export class SlackEvents {
 							modifiedAt: post.createdAt,
 							mostRecentPostCreatedAt: post.createdAt,
 							mostRecentPostId: post.id
-						}
+						},
+						$version: { before: "*" }
 					} as unknown
 				]
 			} as StreamsRTMessage;
@@ -685,7 +696,8 @@ export class SlackEvents {
 								id: this._api.userId,
 								$set: {
 									dnd: e.dnd_status.dnd_enabled
-								}
+								},
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as UsersRTMessage;
@@ -702,7 +714,8 @@ export class SlackEvents {
 								id: e.user,
 								$set: {
 									dnd: e.dnd_status.dnd_enabled
-								}
+								},
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as UsersRTMessage;
@@ -739,7 +752,8 @@ export class SlackEvents {
 											[`preferences.mutedStreams.${streamId}`]: true
 										}),
 										{}
-									)
+									),
+									$version: { before: "*" }
 								} as unknown
 							]
 						} as PreferencesRTMessage;
@@ -758,7 +772,8 @@ export class SlackEvents {
 								id: this._api.userId,
 								$set: {
 									presence: e.presence
-								}
+								},
+								$version: { before: "*" }
 							} as unknown
 						]
 					} as UsersRTMessage;
@@ -774,7 +789,8 @@ export class SlackEvents {
 							id: u,
 							$set: {
 								presence: e.presence
-							}
+							},
+							$version: { before: "*" }
 						}));
 					} else {
 						data = [
@@ -782,7 +798,8 @@ export class SlackEvents {
 								id: e.user,
 								$set: {
 									presence: e.presence
-								}
+								},
+								$version: { before: "*" }
 							}
 						];
 					}

shared/agent/src/api/slack/slackApi.ts
@@ -1822,7 +1822,8 @@ export class SlackApiProvider implements ApiProvider {
 				data: [
 					{
 						id: request.streamId,
-						$pull: { memberIds: [this._slackUserId] }
+						$pull: { memberIds: [this._slackUserId] },
+						$version: { before: "*" }
 					}
 				]
 			});

shared/agent/src/managers/baseManager.ts
@@ -7,7 +7,7 @@ import { BaseCache, KeyValue } from "./cache/baseCache";
 import * as operations from "./operations";
 
 function isDirective(data: any): boolean {
-	return !!data.$version;
+	return Boolean(data.$version);
 }
 
 function isCompatibleVersion(cachedEntity: any, newEntityOrDirective: any): boolean {
@@ -18,7 +18,7 @@ function isCompatibleVersion(cachedEntity: any, newEntityOrDirective: any): bool
 				`Received directive without version attribute for object Id=${newEntityOrDirective.id}`
 			);
 		}
-		return directiveVersion.before === cachedEntity.version;
+		return directiveVersion.before === cachedEntity.version || directiveVersion.before === "*";
 	} else {
 		if (cachedEntity.version == null && newEntityOrDirective.version == null) {
 			return true;