ce412081ee6a3a094e7c0e890779efa21dfe4a3f
Brian Canzanella <brian@codestream.com>
2019-01-03 17:18:12 -0500

    updates build script

--- vs/build/build.ps1
+++ vs/build/build.ps1
@@ -1,8 +1,17 @@
 ﻿param (
-    [string]$configuration = "Release"    
+    [string]$configuration = "Release",
+    [double]$visualStudioVersion = 15.0,
+    [boolean]$deployExtension = $false
 )
 
 #https://stackoverflow.com/questions/42874400/how-to-build-a-visual-studio-2017-vsix-using-msbuild
 $msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\msbuild.exe"
 
-& $msbuild ..\src\CodeStream.VisualStudio.sln /v:normal /target:Clean /target:Build /p:Configuration=$configuration /p:Platform=x86 /p:DeployExtension=false /p:VisualStudioVersion=15.0 /p:OutputPath=artifacts
+# build vscode-codestream (npm run rebuild)
+
+$OutputDir = $($PSScriptRoot+"\artifacts");
+Remove-Item $($PSScriptRoot+"\artifacts\*") -Recurse -Force
+
+# move devteam DotNetBrowser runtime license into ..\src\CodeStream.VisualStudio
+
+& $msbuild ..\src\CodeStream.VisualStudio.sln /v:normal /target:Clean /target:Build /p:Configuration=$configuration /p:Platform=x86 /p:DeployExtension=$deployExtension /p:VisualStudioVersion=$visualStudioVersion /p:OutputPath=$OutputDir
