e5508fbf3947772ee0a90d9e3b19beece5ff25d5
Brian Canzanella <brian@codestream.com>
2019-01-08 11:24:19 -0500

    cleans log before opening log watcher

--- vs/tools/log-watcher-extension.ps1
+++ vs/tools/log-watcher-extension.ps1
@@ -1,2 +1,3 @@
 ﻿#Get-Content "$($env:LOCALAPPDATA)\Codestream\vs-extension.log"  –Wait | where { $_ -match “WARNING” }
+Clear-Content "$($env:LOCALAPPDATA)\Codestream\Logs\vs-extension.log"
 Get-Content "$($env:LOCALAPPDATA)\Codestream\Logs\vs-extension.log"  –Wait
