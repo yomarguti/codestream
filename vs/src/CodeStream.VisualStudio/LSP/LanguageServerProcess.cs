71141d5e4434c7c64bdf3c05c4c6d747167886fa
Eric Amodio <eamodio@gmail.com>
2019-02-04 13:40:34 -0500

    Adds full agent sourcemaps & debugging

--- vs/src/CodeStream.VisualStudio/LSP/LanguageServerProcess.cs
+++ vs/src/CodeStream.VisualStudio/LSP/LanguageServerProcess.cs
@@ -5,35 +5,35 @@ using CodeStream.VisualStudio.Core.Logging;
 
 namespace CodeStream.VisualStudio.LSP
 {
-    public interface ILanguageServerProcess
-    {
-        System.Diagnostics.Process Create(TraceLevel traceLevel);
-    }
+	public interface ILanguageServerProcess
+	{
+		System.Diagnostics.Process Create(TraceLevel traceLevel);
+	}
 
-    public class LanguageServerProcess : ILanguageServerProcess
-    {
-        /// <summary>
-        /// Creates the lsp server process object
-        /// </summary>
-        /// <returns></returns>
-        public System.Diagnostics.Process Create(TraceLevel traceLevel)
-        {
-            var assembly = Assembly.GetAssembly(typeof(LanguageServerProcess));
-            string arguments = null;
-            var exe = @"node.exe";
-            var logPath = $"{Application.LogPath}vs-agent.log";
+	public class LanguageServerProcess : ILanguageServerProcess
+	{
+		/// <summary>
+		/// Creates the lsp server process object
+		/// </summary>
+		/// <returns></returns>
+		public System.Diagnostics.Process Create(TraceLevel traceLevel)
+		{
+			var assembly = Assembly.GetAssembly(typeof(LanguageServerProcess));
+			string arguments = null;
+			var exe = @"node.exe";
+			var logPath = $"{Application.LogPath}vs-agent.log";
 #if DEBUG
             var path = Path.GetDirectoryName(assembly.Location) + @"\LSP\agent.js";
-            arguments = $@"--nolazy --inspect=6009 ""{path}"" --stdio --log={logPath}";
+            arguments = $@"--nolazy --inspect=6010 ""{path}"" --stdio --log={logPath}";
 #else
-            exe = Path.GetDirectoryName(assembly.Location) + @"\LSP\agent.exe";
-            arguments = $@"--stdio --nolazy";
-            if (traceLevel == TraceLevel.Verbose)
-            {
-                arguments += $" --log={logPath}";
-            }
+			exe = Path.GetDirectoryName(assembly.Location) + @"\LSP\agent.exe";
+			arguments = $@"--stdio --nolazy";
+			if (traceLevel == TraceLevel.Verbose)
+			{
+				arguments += $" --log={logPath}";
+			}
 #endif
-            return ProcessFactory.Create(exe, arguments);
-        }
-    }
+			return ProcessFactory.Create(exe, arguments);
+		}
+	}
 }
