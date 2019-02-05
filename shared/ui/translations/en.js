c005bd1cab478cef7268fee3ec55d2cecba6394e
akonwi <akonwi@codestream.com>
2019-02-05 13:47:59 -0500

    change translations from json to module

--- shared/ui/translations/en.js
+++ shared/ui/translations/en.js
@@ -1,13 +1,14 @@
-{
-	"and": "and",
-	"learnMore": "Learn more.",
+export default {
+	and: "and",
+	learnMore: "Learn more.",
 	"signUp.username.label": "Username",
 	"signUp.username.tip": "Up to 21 characters. Valid special characters are (._-)",
 	"signUp.username.length": "Up to 21 characters",
 	"signUp.username.validCharacters": "Invalid character",
 	"signUp.username.alreadyTaken": "Sorry, already taken.",
 	"signUp.password.label": "Password",
-	"signUp.password.tooShort": "{countNeeded, number} more {countNeeded, plural, one {char} other {chars}} plz",
+	"signUp.password.tooShort":
+		"{countNeeded, number} more {countNeeded, plural, one {char} other {chars}} plz",
 	"signUp.password.help": "6+ characters",
 	"signUp.password.tip": "6+ characters",
 	"signUp.email.label": "Email",
@@ -18,18 +19,22 @@
 	"signUp.legal.start": "By signing up, you agree to the",
 	"signUp.legal.termsOfService": "Terms of Service",
 	"signUp.legal.privacyPolicy": "Privacy Policy",
-	"signUp.legal.telemetryConsent": "Help CodeStream improve by allowing the use of 3rd-party analytics tools.",
+	"signUp.legal.telemetryConsent":
+		"Help CodeStream improve by allowing the use of 3rd-party analytics tools.",
 	"signUp.legal.learnMore": "Learn more.",
 	"signUp.footer.alreadySignedUp": "Have an account?",
 	"signUp.footer.signIn": "Sign In",
-	"signup.complete.withSlack": "Your web browser should have opened up to a Slack authentication page. Once you've completed the authentication process, return here to get started with CodeStream.",
-	"signup.complete.main": "Your web browser should have opened up to a CodeStream registration page. Once you've completed the sign-up process, return here to get started with CodeStream.",
+	"signup.complete.withSlack":
+		"Your web browser should have opened up to a Slack authentication page. Once you've completed the authentication process, return here to get started with CodeStream.",
+	"signup.complete.main":
+		"Your web browser should have opened up to a CodeStream registration page. Once you've completed the sign-up process, return here to get started with CodeStream.",
 	"signup.complete.button": "CONTINUE",
 	"confirmation.invalid": "Uh oh. Invalid code.",
 	"confirmation.expired": "Sorry, that code has expired.",
 	"confirmation.emailSent": "Email Sent!",
 	"confirmation.header": "You're almost there!",
-	"confirmation.instructions": "Please check your email. We've sent you a 6-digit code to confirm your email address.",
+	"confirmation.instructions":
+		"Please check your email. We've sent you a 6-digit code to confirm your email address.",
 	"confirmation.didNotReceive": "Didn't receive it? Check your spam folder, or have us",
 	"confirmation.sendAnother": "send another email.",
 	"confirmation.incorrectEmail": "{email} not correct?",
@@ -47,8 +52,10 @@
 	"login.footer.signUp": "Sign Up",
 	"login.password.label": "Password",
 	"noGit.header": "Git Issue",
-	"noGit.message": "CodeStream only works with projects controlled by Git and you don't appear to have any currently open.",
-	"noGit.message2": "Make sure the root directory you have open is an actual Git repository, and not a directory containing repositories.",
+	"noGit.message":
+		"CodeStream only works with projects controlled by Git and you don't appear to have any currently open.",
+	"noGit.message2":
+		"Make sure the root directory you have open is an actual Git repository, and not a directory containing repositories.",
 	"createTeam.header": "Create a Team",
 	"createTeam.info": "Your company or organization name would fit the bill here.",
 	"createTeam.additionalInfo": "Note that a team can own multiple repositories.",
@@ -57,25 +64,32 @@
 	"teamSelection.error.noPermission": "You are not a member of the selected team.",
 	"teamSelection.error.teamNotFound": "The selected team doesn't exist.",
 	"teamSelection.backToInvite": "Go Back",
-	"slackInfo.p1": "Connect CodeStream to Slack and all of your team's messages on CodeStream will also get posted to a channel on Slack.",
-	"slackInfo.p2": "This is a great option if you have developers that use an IDE not currently supported by CodeStream. They can even reply to messages directly from Slack!",
+	"slackInfo.p1":
+		"Connect CodeStream to Slack and all of your team's messages on CodeStream will also get posted to a channel on Slack.",
+	"slackInfo.p2":
+		"This is a great option if you have developers that use an IDE not currently supported by CodeStream. They can even reply to messages directly from Slack!",
 	"getInvited.header": "Get Invited?",
-	"getInvited.text": "Looks like there are already repos from the {fromWhat} connected to CodeStream. ",
+	"getInvited.text":
+		"Looks like there are already repos from the {fromWhat} connected to CodeStream. ",
 	"getInvited.afterInvite": "Got my invitation. Let's go!",
 	"getInvited.or": "or",
-	"getInvited.contact.oneTeam": "If this repo should be connected to the {teamName} team as well, contact {creatorName} to get invited.",
-	"getInvited.contact.multipleTeams": "If this repo should be connected to one of the following teams, please ask to be invited: {teams}.",
+	"getInvited.contact.oneTeam":
+		"If this repo should be connected to the {teamName} team as well, contact {creatorName} to get invited.",
+	"getInvited.contact.multipleTeams":
+		"If this repo should be connected to one of the following teams, please ask to be invited: {teams}.",
 	"getInvited.selectTeam": "I don't want to join that team.",
 	"getInvited.createTeam": "I want to create a team.",
 	"teamMemberSelection.submitButton": "GET STARTED",
 	"teamMemberSelection.header": "Who's on the team?",
 	"teamMemberSelection.skip": "Skip This",
-	"teamMemberSelection.existingTeam.peopleToAdd": "Here are some people that have committed to this repo that you might want to add.",
-	"teamMemberSelection.newTeam.peopleToAdd": "Here are some suggestions pulled from the repo's commit history.",
+	"teamMemberSelection.existingTeam.peopleToAdd":
+		"Here are some people that have committed to this repo that you might want to add.",
+	"teamMemberSelection.newTeam.peopleToAdd":
+		"Here are some suggestions pulled from the repo's commit history.",
 	"teamMemberSelection.anyoneMissing": "Anyone missing?",
 	"teamMemberSelection.addThem": "Add them!",
 	"teamMemberSelection.add": "ADD",
 	"teamMemberSelection.invite": "Invite",
 	"createChannel.name.invalid": "Looks invalid?",
 	"createChannel.name.required": "Required"
-}
+};
