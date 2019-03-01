9860068361f537ea459f958a5375167f94b063f7
Brian Canzanella <brian@codestream.com>
2019-03-01 14:21:09 -0500

    adds service locator

---
+++ vs/src/CodeStream.VisualStudio/ServiceLocator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.Shell;
+
+namespace CodeStream.VisualStudio
+{
+    public static class ServiceLocator
+    {
+        /// <summary>
+        /// GetGlobalService from Package
+        /// </summary>
+        /// <typeparam name="S">The S version of the interface</typeparam>
+        /// <typeparam name="I">The I version of the interface</typeparam>
+        /// <returns></returns>
+        public static I Get<S, I>()
+        {
+            return (I)Package.GetGlobalService(typeof(S));
+        }
+    }
+}
