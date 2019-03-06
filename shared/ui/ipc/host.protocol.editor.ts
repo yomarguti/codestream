7a742f76a05c7132082862e36be48f3ea42e56d4
Eric Amodio <eric@codestream.com>
2019-03-05 19:21:43 -0500

    Removes deprecated requests

--- shared/ui/ipc/host.protocol.editor.ts
+++ shared/ui/ipc/host.protocol.editor.ts
@@ -1,4 +1,3 @@
-import { CSMarker } from "@codestream/protocols/api";
 import { RequestType } from "vscode-jsonrpc";
 import { Range } from "vscode-languageserver-types";
 import { IpcRoutes } from "./webview.protocol.common";
@@ -16,40 +15,6 @@ export const EditorHighlightRangeRequestType = new RequestType<
 	void,
 	void
 >(`${IpcRoutes.Host}/editor/highlight/range`);
-
-// DEPRECATED: use EditorHighlightRangeRequestType
-export interface EditorHighlightLineRequest {
-	uri: string;
-	line: number;
-	highlight: boolean;
-	source: string;
-}
-export interface EditorHighlightLineResponse {
-	result: string | undefined;
-}
-export const EditorHighlightLineRequestType = new RequestType<
-	EditorHighlightLineRequest,
-	EditorHighlightLineResponse,
-	void,
-	void
->(`${IpcRoutes.Host}/editor/highlight/line`);
-
-// DEPRECATED: use EditorHighlightRangeRequestType
-export interface EditorHighlightMarkerRequest {
-	uri: string;
-	marker: CSMarker;
-	highlight: boolean;
-	source: string;
-}
-export interface EditorHighlightMarkerResponse {
-	result: string | undefined;
-}
-export const EditorHighlightMarkerRequestType = new RequestType<
-	EditorHighlightMarkerRequest,
-	EditorHighlightMarkerResponse,
-	void,
-	void
->(`${IpcRoutes.Host}/editor/highlight/marker`);
 
 export enum EditorRevealRangeResult {
 	Success = "SUCCESS",
@@ -72,30 +37,3 @@ export const EditorRevealRangeRequestType = new RequestType<
 	void,
 	void
 >(`${IpcRoutes.Host}/editor/reveal/range`);
-
-// DEPRECATED: use EditorRevealRangeRequestType
-export interface EditorRevealLineRequest {
-	line: number;
-}
-export interface EditorRevealLineResponse {}
-export const EditorRevealLineRequestType = new RequestType<
-	EditorRevealLineRequest,
-	EditorRevealLineResponse,
-	void,
-	void
->(`${IpcRoutes.Host}/editor/reveal/line`);
-
-// DEPRECATED: use EditorRevealRangeRequestType
-export interface EditorRevealMarkerRequest {
-	marker: CSMarker;
-	preserveFocus: boolean;
-}
-export interface EditorRevealMarkerResponse {
-	result: string | undefined;
-}
-export const EditorRevealMarkerRequestType = new RequestType<
-	EditorRevealMarkerRequest,
-	EditorRevealMarkerResponse,
-	void,
-	void
->(`${IpcRoutes.Host}/editor/reveal/marker`);
