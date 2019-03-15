89488d806f2196c64bbceccc84d952faaf8ab739
akonwi <akonwi@codestream.com>
2019-03-15 11:23:44 -0400

    send `DidChangeDocumentMarkersNotification` on file changes
    https://trello.com/c/aGilYnSa/1235-spatial-codemarks-dont-move-when-you-add-code

--- shared/agent/src/documentManager.ts
+++ shared/agent/src/documentManager.ts
@@ -6,27 +6,32 @@ import {
 	TextDocumentChangeEvent,
 	TextDocuments
 } from "vscode-languageserver";
+import { DidChangeDocumentMarkersNotificationType } from "./protocol/agent.protocol";
 import { Disposables } from "./system";
 
 export class DocumentManager implements Disposable {
 	private readonly _disposable: Disposable;
 
-	constructor(private readonly _documents: TextDocuments, connection: Connection) {
+	constructor(private readonly _documents: TextDocuments, private _connection: Connection) {
 		this._disposable = Disposables.from(
-			this._documents.onDidOpen(this.onOpened),
+			// this._documents.onDidOpen(this.onOpened),
 			this._documents.onDidChangeContent(this.onContentChanged)
 		);
 
-		this._documents.listen(connection);
+		this._documents.listen(_connection);
 	}
 
 	dispose() {
 		this._disposable && this._disposable.dispose();
 	}
 
-	private onContentChanged(e: TextDocumentChangeEvent) {}
+	private onContentChanged = (e: TextDocumentChangeEvent) => {
+		this._connection.sendNotification(DidChangeDocumentMarkersNotificationType, {
+			textDocument: { uri: e.document.uri }
+		});
+	}
 
-	private onOpened(e: TextDocumentChangeEvent) {}
+	// private onOpened(e: TextDocumentChangeEvent) {}
 
 	get(uri: string): TextDocument | undefined {
 		return this._documents.get(uri);
