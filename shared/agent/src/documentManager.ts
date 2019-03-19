57ff37f77aa5901871e30635bea1ef5da52ead54
Eric Amodio <eric@codestream.com>
2019-03-19 19:04:11 -0400

    Normalizes agent document manager lookups

--- shared/agent/src/documentManager.ts
+++ shared/agent/src/documentManager.ts
@@ -9,10 +9,18 @@ import {
 import { DidChangeDocumentMarkersNotificationType } from "./protocol/agent.protocol";
 import { Disposables } from "./system";
 
+const escapedRegex = /(^.*?:\/\/\/)([a-z])%3A(\/.*$)/;
+const unescapedRegex = /(^.*?:\/\/\/)([a-zA-Z]):(\/.*$)/;
+
 export class DocumentManager implements Disposable {
 	private readonly _disposable: Disposable;
+	private readonly _isWindows: boolean;
+
+	private readonly _normalizedUriLookup = new Map<string, string>();
 
 	constructor(private readonly _documents: TextDocuments, private _connection: Connection) {
+		this._isWindows = process.platform === "win32";
+
 		this._disposable = Disposables.from(
 			// this._documents.onDidOpen(this.onOpened),
 			this._documents.onDidChangeContent(this.onContentChanged)
@@ -34,6 +42,65 @@ export class DocumentManager implements Disposable {
 	// private onOpened(e: TextDocumentChangeEvent) {}
 
 	get(uri: string): TextDocument | undefined {
-		return this._documents.get(uri);
+		if (!this._isWindows) {
+			return this._documents.get(uri);
+		}
+
+		// If we are on windows we have to do some drive letter manipulation to support different editor using different uri formatting
+		const key = this._normalizedUriLookup.get(uri);
+		if (key !== undefined) {
+			return this._documents.get(key);
+		}
+
+		let doc = this._documents.get(uri);
+		if (doc !== undefined) return doc;
+
+		let match = unescapedRegex.exec(uri);
+		if (match != null) {
+			const escapedUri = uri.replace(unescapedRegex, function(
+				_,
+				start: string,
+				drive: string,
+				end: string
+			) {
+				return `${start}${drive.toLowerCase()}%3A${end}`;
+			});
+			doc = this._documents.get(escapedUri);
+			if (doc !== undefined) {
+				this._normalizedUriLookup.set(uri, doc.uri);
+			}
+
+			return doc;
+		}
+
+		match = escapedRegex.exec(uri);
+		if (match != null) {
+			let unescapedUri = uri.replace(escapedRegex, function(
+				_,
+				start: string,
+				drive: string,
+				end: string
+			) {
+				return `${start}${drive.toUpperCase()}:${end}`;
+			});
+			doc = this._documents.get(unescapedUri);
+			if (doc !== undefined) {
+				this._normalizedUriLookup.set(uri, doc.uri);
+
+				return doc;
+			}
+
+			unescapedUri = uri.replace(escapedRegex, function(start, drive: string, end) {
+				return `${start}${drive.toLowerCase()}:${end}`;
+			});
+			doc = this._documents.get(unescapedUri);
+			if (doc !== undefined) {
+				this._normalizedUriLookup.set(uri, doc.uri);
+			}
+
+			return doc;
+		}
+
+		return undefined;
 	}
 }
