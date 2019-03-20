2adffebd2a3b3d7d6dbc08525462738e836935e9
James Price <jimmyjayp@gmail.com>
2019-03-20 12:50:27 -0400

    Publish to mktplace script

--- vs/build/publish-to-marketplace.ps1
+++ vs/build/publish-to-marketplace.ps1
@@ -3,6 +3,7 @@ param([string] $checkoutDir = $pwd, [string] $assetEnv = "", [string] $buildNumb
 Write-Host '**** The script is running in directory' (Get-Location)
 $buildDir = $checkoutDir + '\vs-codestream\build'
 $assetDir = $buildDir + '\artifacts\x86\Release'
+$vsce = $checkoutDir + '\vs-codestream\node_modules\.bin\vsce'
 
 $asset = $assetDir + '\codestream-vs-' + $buildNumber + '.vsix'
 Write-Host 'Here is the VSIX file (' $asset '):'
@@ -12,5 +13,6 @@ $assetInfo = $assetDir + '\codestream-vs-' + $buildNumber + '.info'
 Write-Host 'Here is the VSIX file (' $assetInfo '):'
 Get-ChildItem $assetInfo
 
-# Write-Host 'Publishing asset to marketplace'
-# Write-Host '\vs-codestream\node_modules\.bin\vsce publish --packagePath ' $asset
+Write-Host 'Publishing asset to marketplace'
+Write-Host "$vsce publish --packagePath $asset"
+# $vsce publish --packagePath $asset
