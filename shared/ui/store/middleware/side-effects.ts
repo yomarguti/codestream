47e3f635960ffdb12b3fa0e3fb35ba289c996205
akonwi <akonwi@codestream.com>
2019-03-26 10:12:56 -0400

    unset `context.issueProvider` if it is a string

--- shared/ui/store/middleware/side-effects.ts
+++ shared/ui/store/middleware/side-effects.ts
@@ -3,6 +3,7 @@ import { BootstrapActionType } from "../actions";
 import { Middleware } from "redux";
 import { fetchCodemarks } from "@codestream/webview/Stream/actions";
 import { State as SessionState } from "../session/types";
+import { setIssueProvider } from "../context/actions";
 
 // Do stuff based on data and not UI details
 export const sideEffects: Middleware = ({ dispatch, getState }) => next => action => {
@@ -14,6 +15,12 @@ export const sideEffects: Middleware = ({ dispatch, getState }) => next => actio
 				// TODO: redundant calls can be made by both the components that need the data and here
 				fetchCodemarks()(dispatch);
 			});
+
+			// This can be deleted in the future. It's purpose is to invalidate `context.issueProvider`
+			// as a string because the data structure has changed
+			if (typeof getState().context.issueProvider === "string") {
+				dispatch(setIssueProvider(undefined));
+			}
 		}
 	}
 	return next(action);
