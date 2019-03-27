8b0eb5a470ce3f5c7e2c1ba73150b1e301a3724b
Eric Amodio <eric@codestream.com>
2019-03-27 00:26:30 -0400

    Aligns with other extension projects

--- vscode/src/webviews/app/index.ts
+++ vscode/src/webviews/app/index.ts
@@ -5,19 +5,11 @@ import { initializeColorPalette } from "./theme";
 
 declare function acquireVsCodeApi();
 
-const vscodeApi = acquireVsCodeApi();
+const api = acquireVsCodeApi();
 const channel = new MessageChannel();
 
-window.addEventListener(
-	"message",
-	message => {
-		channel.port1.postMessage(message.data);
-	},
-	false
-);
-channel.port1.onmessage = message => {
-	vscodeApi.postMessage(message.data);
-};
+window.addEventListener("message", message => channel.port1.postMessage(message.data), false);
+channel.port1.onmessage = message => api.postMessage(message.data);
 
 setupCommunication(channel.port2);
 initializeColorPalette();
