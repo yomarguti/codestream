024d8692d0d1034264398a395916cd11584eba38
Brian Canzanella <brian@codestream.com>
2019-03-28 20:23:03 -0400

    fixes DocumentMarkerChangedEvents locking up the IDE

--- vs/src/CodeStream.VisualStudio/LSP/CustomMessageHandler.cs
+++ vs/src/CodeStream.VisualStudio/LSP/CustomMessageHandler.cs
@@ -1,120 +1,106 @@
-﻿using CodeStream.VisualStudio.Core.Logging;
+﻿using CodeStream.VisualStudio.Core;
+using CodeStream.VisualStudio.Core.Logging;
 using CodeStream.VisualStudio.Events;
 using CodeStream.VisualStudio.Extensions;
 using CodeStream.VisualStudio.Models;
 using CodeStream.VisualStudio.Services;
-using Microsoft.VisualStudio.Shell;
 using Newtonsoft.Json.Linq;
 using Serilog;
 using StreamJsonRpc;
 // ReSharper disable UnusedMember.Global
 
-namespace CodeStream.VisualStudio.LSP
-{
-    internal class CustomMessageHandler
-    {
-        private static readonly ILogger Log = LogManager.ForContext<CustomMessageHandler>();
+namespace CodeStream.VisualStudio.LSP {
+	internal class CustomMessageHandler {
+		private static readonly ILogger Log = LogManager.ForContext<CustomMessageHandler>();
 
-        private readonly IEventAggregator _eventAggregator;
-        private readonly IWebviewIpc _ipc;
+		private readonly IEventAggregator _eventAggregator;
+		private readonly IWebviewIpc _ipc;
+		private readonly DebounceDispatcher _documentMarkersDispatcher = new DebounceDispatcher();
 
-        public CustomMessageHandler(IEventAggregator eventAggregator, IWebviewIpc ipc)
-        {
-            _eventAggregator = eventAggregator;
-            _ipc = ipc;
-        }
+		public CustomMessageHandler(IEventAggregator eventAggregator, IWebviewIpc ipc) {
+			_eventAggregator = eventAggregator;
+			_ipc = ipc;
+		}
 
-        [JsonRpcMethod(DidChangeDataNotificationType.MethodName)]
-        public void OnDidChangeData(JToken e)
-        {
-            _ipc.Notify(new DidChangeDataNotificationType(e));
-        }
+		[JsonRpcMethod(DidChangeDataNotificationType.MethodName)]
+		public void OnDidChangeData(JToken e) {
+			_ipc.Notify(new DidChangeDataNotificationType(e));
+		}
 
-        [JsonRpcMethod(DidChangeConnectionStatusNotificationType.MethodName)]
-        public void OnDidChangeConnectionStatus(JToken e)
-        {
-            var @params = e.ToObject<DidChangeConnectionStatusNotification>();
+		[JsonRpcMethod(DidChangeConnectionStatusNotificationType.MethodName)]
+		public void OnDidChangeConnectionStatus(JToken e) {
+			var @params = e.ToObject<DidChangeConnectionStatusNotification>();
 
-            Log.Verbose($"{nameof(OnDidChangeConnectionStatus)} {@params.Status}");
+			Log.Verbose($"{nameof(OnDidChangeConnectionStatus)} {@params.Status}");
 
-            switch (@params.Status)
-            {
-                case ConnectionStatus.Disconnected:
-                {
-                    // TODO: Handle this
-                    break;
-                }
-                case ConnectionStatus.Reconnecting:
-                    _ipc.Notify(new DidChangeConnectionStatusNotificationType(@params));
-                    break;
-                case ConnectionStatus.Reconnected:
-                    {
-                        if (@params.Reset == true)
-                        {
-                            _ipc.BrowserService.ReloadWebView();
-                            return;
-                        }
+			switch (@params.Status) {
+				case ConnectionStatus.Disconnected: {
+						// TODO: Handle this
+						break;
+					}
+				case ConnectionStatus.Reconnecting:
+					_ipc.Notify(new DidChangeConnectionStatusNotificationType(@params));
+					break;
+				case ConnectionStatus.Reconnected: {
+						if (@params.Reset == true) {
+							_ipc.BrowserService.ReloadWebView();
+							return;
+						}
 
-                        _ipc.Notify(new DidChangeConnectionStatusNotificationType(@params));
-                        break;
-                    }
-                default:
-                    {
-                        break;
-                    }
-            }
-        }
+						_ipc.Notify(new DidChangeConnectionStatusNotificationType(@params));
+						break;
+					}
+				default: {
+						break;
+					}
+			}
+		}
 
-        [JsonRpcMethod(DidChangeDocumentMarkersNotificationType.MethodName)]
-        public void OnDidChangeDocumentMarkers(JToken e)
-        {
-            var @params = e.ToObject<DidChangeDocumentMarkersNotification>();
+		[JsonRpcMethod(DidChangeDocumentMarkersNotificationType.MethodName)]
+		public void OnDidChangeDocumentMarkers(JToken e) {
+			_documentMarkersDispatcher.Debounce(100, o => {
+				var @params = e.ToObject<DidChangeDocumentMarkersNotification>();
 
-            Log.Verbose($"{nameof(OnDidChangeDocumentMarkers)} {@params?.TextDocument?.Uri}");
+				if (@params == null) {
+					Log.Warning($"{nameof(@params)} is null");
+					return;
+				}
 
-            var uriString = @params.TextDocument.Uri;
-            _ipc.Notify(new DidChangeDocumentMarkersNotificationType
-            {
-                Params = new DidChangeDocumentMarkersNotification
-                {
-                    TextDocument = new TextDocumentIdentifier
-                    {
-                        Uri =  uriString
-                    }
-                }
-            });
+				Log.Verbose($"{nameof(OnDidChangeDocumentMarkers)} {@params?.TextDocument?.Uri}");
 
-            ThreadHelper.JoinableTaskFactory.Run(async delegate
-            {
-                await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
-                _eventAggregator.Publish(new DocumentMarkerChangedEvent
-                {
-                    Uri = uriString.ToUri()
-                });
-            });
-        }
+				var uriString = @params.TextDocument.Uri;
+				_ipc.Notify(new DidChangeDocumentMarkersNotificationType {
+					Params = new DidChangeDocumentMarkersNotification {
+						TextDocument = new TextDocumentIdentifier {
+							Uri = uriString
+						}
+					}
+				});
 
-        [JsonRpcMethod(DidChangeVersionCompatibilityNotificationType.MethodName)]
-        public void OnDidChangeVersionCompatibility(JToken e)
-        {
-            Log.Verbose($"{nameof(OnDidChangeVersionCompatibility)}");
+				_eventAggregator.Publish(new DocumentMarkerChangedEvent {
+					Uri = uriString.ToUri()
+				});
+			});
+		}
 
-            _ipc.Notify(new DidChangeVersionCompatibilityNotificationType(e));
-        }
+		[JsonRpcMethod(DidChangeVersionCompatibilityNotificationType.MethodName)]
+		public void OnDidChangeVersionCompatibility(JToken e) {
+			Log.Verbose($"{nameof(OnDidChangeVersionCompatibility)}");
 
-        [JsonRpcMethod(DidLogoutNotificationType.MethodName)]
-        public void OnDidLogout(JToken e)
-        {
-            var @params = e.ToObject<DidLogoutNotification>();
+			_ipc.Notify(new DidChangeVersionCompatibilityNotificationType(e));
+		}
 
-            Log.Verbose($"{nameof(OnDidLogout)} {@params.Reason}");
+		[JsonRpcMethod(DidLogoutNotificationType.MethodName)]
+		public void OnDidLogout(JToken e) {
+			var @params = e.ToObject<DidLogoutNotification>();
 
-            _eventAggregator.Publish(new AuthenticationChangedEvent { Reason = @params.Reason });
+			Log.Verbose($"{nameof(OnDidLogout)} {@params.Reason}");
 
-            _ipc.Notify(new DidLogoutNotificationType
-            {
-                Params = @params
-            });
-        }
-    }
+			_eventAggregator.Publish(new AuthenticationChangedEvent { Reason = @params.Reason });
+
+			_ipc.Notify(new DidLogoutNotificationType {
+				Params = @params
+			});
+		}
+	}
 }
