3d7e30c1c0986ff361a3b347441811105ef6c6c3
akonwi <akonwi@codestream.com>
2019-04-16 09:43:15 -0400

    logic is hard

--- shared/ui/Stream/index.js
+++ shared/ui/Stream/index.js
@@ -1863,7 +1863,7 @@ export class SimpleStream extends Component {
 							"This repo doesn’t have a remote URL configured. When your teammates view this post, we won’t be able to connect the code block to the appropriate file in their IDE."
 					};
 				}
-			} else if (scmInfo.error) {
+			} else {
 				warning = {
 					title: "Missing Git Info",
 					message:
