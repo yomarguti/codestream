8bf396f1302bc62962aa5c6a7bf26fa534566cbf
Brian Canzanella <brian@codestream.com>
2019-05-13 18:09:15 -0400

    another opener script

---
+++ vs/tools/opener--github4vs.ps1
@@ -0,0 +1,16 @@
+﻿Get-Process devenv -ErrorAction SilentlyContinue | Stop-Process
+Write-Host "Starting at $(Get-Date)"
+
+$counter = 0;
+while($true) { 
+    start "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" "..\..\..\GitHubForVisualStudio\GitHubVS.sln"
+    Start-Sleep -Seconds 1
+    start "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" "..\..\..\GitHubForVisualStudio\GitHubVS.sln"
+    Start-Sleep -Seconds 60
+    
+    Get-Process devenv -ErrorAction SilentlyContinue | Stop-Process
+    Start-Sleep -Seconds 15
+    $counter = $counter+2;
+
+    Write-Host "$($counter) times at $(Get-Date)"
+}
