caacf070ac628902c8bc4df3033211a7e8442aa7
Brian Canzanella <brian@codestream.com>
2019-06-24 15:13:02 -0400

    fixes a bug with missing ProviderType

--- vs/src/CodeStream.VisualStudio/Models/Types.cs
+++ vs/src/CodeStream.VisualStudio/Models/Types.cs
@@ -1,83 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CodeStream.VisualStudio.Core;
+﻿using CodeStream.VisualStudio.Core;
 using Newtonsoft.Json;
 
-namespace CodeStream.VisualStudio.Models
-{
-    public enum LoginResult
-    {
-        // ReSharper disable InconsistentNaming
-        SUCCESS,
-        INVALID_CREDENTIALS,
-        TOKEN_INVALID,
-        NOT_CONFIRMED,
-        USER_NOT_ON_TEAM,
-        UNKNOWN,
-        VERSION_UNSUPPORTED
-        // ReSharper restore InconsistentNaming
-    }
+namespace CodeStream.VisualStudio.Models {
+	public enum LoginResult {
+		// ReSharper disable InconsistentNaming
+		SUCCESS,
+		INVALID_CREDENTIALS,
+		TOKEN_INVALID,
+		NOT_CONFIRMED,
+		USER_NOT_ON_TEAM,
+		UNKNOWN,
+		VERSION_UNSUPPORTED
+		// ReSharper restore InconsistentNaming
+	}
 
-    [JsonConverter(typeof(CamelCaseStringEnumConverter))]
-    public enum LogoutReason
-    {
-        Token,
-        Unknown
-    }
+	[JsonConverter(typeof(CamelCaseStringEnumConverter))]
+	public enum LogoutReason {
+		Token,
+		Unknown
+	}
 
-    public enum ProviderType
-    {
-        Slack
-    }
+	public enum ProviderType {
+		MSTeams,
+		Slack
+	}
 
-    [JsonConverter(typeof(CamelCaseStringEnumConverter))]
-    public enum CodemarkType
-    {
-        Comment,
-        Issue,
-        Bookmark,
-        Question,
-        Trap,
-        Link
-    }
+	[JsonConverter(typeof(CamelCaseStringEnumConverter))]
+	public enum CodemarkType {
+		Comment,
+		Issue,
+		Bookmark,
+		Question,
+		Trap,
+		Link
+	}
 
-    [JsonConverter(typeof(CamelCaseStringEnumConverter))]
-    public enum ConnectionStatus
-    {
-        Disconnected,
-        Reconnected,
-        Reconnecting,
-    }
+	[JsonConverter(typeof(CamelCaseStringEnumConverter))]
+	public enum ConnectionStatus {
+		Disconnected,
+		Reconnected,
+		Reconnecting,
+	}
 
-    public enum StreamType
-    {
-        channel,
-        direct,
-        file
-    }
+	public enum StreamType {
+		channel,
+		direct,
+		file
+	}
 
-    //export enum MarkerNotLocatedReason
-    //{
-    //    MISSING_ORIGINAL_LOCATION = "missing original location",
-    //    MISSING_ORIGINAL_COMMIT = "missing original commit",
-    //    CODEBLOCK_DELETED = "code block deleted",
-    //    UNKNOWN = "unknown"
-    //}
+	//export enum MarkerNotLocatedReason
+	//{
+	//    MISSING_ORIGINAL_LOCATION = "missing original location",
+	//    MISSING_ORIGINAL_COMMIT = "missing original commit",
+	//    CODEBLOCK_DELETED = "code block deleted",
+	//    UNKNOWN = "unknown"
+	//}
 
-    public static class ChangeDataType
-    {
-        public const string Codemarks = "codemarks";
-        public const string MarkerLocations = "markerLocations";
-        public const string Markers = "markers";
-        public const string Posts = "posts";
-        public const string Preferences = "preferences";
-        public const string Repositories = "repos";
-        public const string Streams = "streams";
-        public const string Teams = "teams";
-        public const string Unreads = "unreads";
-        public const string Users = "users";
-    }
+	public static class ChangeDataType {
+		public const string Codemarks = "codemarks";
+		public const string MarkerLocations = "markerLocations";
+		public const string Markers = "markers";
+		public const string Posts = "posts";
+		public const string Preferences = "preferences";
+		public const string Repositories = "repos";
+		public const string Streams = "streams";
+		public const string Teams = "teams";
+		public const string Unreads = "unreads";
+		public const string Users = "users";
+	}
 }
