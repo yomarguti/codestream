5a2f81c1e83a6ffc216fb20008792746fe243074
Marcelo Bukowski de Farias <marcelo@codestream.com>
2019-06-25 13:28:55 -0400

    Sort Asana projects in alphabetic order

--- shared/agent/src/providers/asana.ts
+++ shared/agent/src/providers/asana.ts
@@ -86,6 +86,18 @@ export class AsanaProvider extends ThirdPartyProviderBase<CSAsanaProviderInfo> {
 			boards.push(board);
 		}
 
+		boards.sort((a, b) => {
+			const nameA = a.name.toLowerCase();
+			const nameB = b.name.toLowerCase();
+			if (nameA < nameB) {
+				return -1;
+			}
+			if (nameA > nameB) {
+				return 1;
+			}
+			return 0;
+		});
+
 		return { boards };
 	}
 
