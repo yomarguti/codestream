ba8ae2108aff91c5799be1feb411d2a19feefe9e
akonwi <akonwi@codestream.com>
2019-06-26 11:28:41 -0400

    polyfill localStorage if necessary

--- shared/ui/utilities/storage.ts
+++ shared/ui/utilities/storage.ts
@@ -1,3 +1,21 @@
+function polyfillLocalStorage() {
+	const _localStorage = {};
+	// not all methods are implemented, just enough for now
+	Object.defineProperty(window, "localStorage", {
+		value: {
+			setItem(key: string, value: string) {
+				_localStorage[key] = value;
+			},
+			getItem(key: string) {
+				return _localStorage[key];
+			},
+			removeItem(key: string) {
+				delete _localStorage[key];
+			}
+		}
+	});
+}
+
 class LocalStore {
 	set(key: string, value: any) {
 		localStorage.setItem(key, JSON.stringify(value));
@@ -14,6 +32,14 @@ class LocalStore {
 
 	delete(key: string) {
 		localStorage.removeItem(key);
+	}
+}
+
+try {
+	window.localStorage.getItem("test");
+} catch (error) {
+	if (error.message.includes("Failed to read the 'localStorage' property from 'Window'")) {
+		polyfillLocalStorage();
 	}
 }
 
