a3a9a8cee0f34d3c42949ab0a425508ce67733d4
Marcelo Bukowski de Farias <marcelo@codestream.com>
2019-07-19 14:01:34 -0400

    Add the ability to debug webview and agent in production builds

--- jb/src/main/kotlin/Globals.kt
+++ jb/src/main/kotlin/Globals.kt
@@ -5,3 +5,4 @@ import com.google.gson.Gson
 val gson = Gson()
 val DEBUG =
     java.lang.management.ManagementFactory.getRuntimeMXBean().inputArguments.toString().contains("-agentlib:jdwp")
+        || System.getProperty("com.codestream.debug")?.equals("true") ?: false
