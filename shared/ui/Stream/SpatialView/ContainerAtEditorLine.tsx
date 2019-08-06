3df1603ec0f38ad02c36c4c47f90aba935555ae9
akonwi <akonwi@codestream.com>
2019-08-06 12:31:47 -0400

    reposition lower codemark containers in spatial view
    https://trello.com/c/SI2uZYYU/2104-reposition-codemarks-expanded-towards-bottom-of-viewport-so-they-are-fully-visible

--- shared/ui/Stream/SpatialView/ContainerAtEditorLine.tsx
+++ shared/ui/Stream/SpatialView/ContainerAtEditorLine.tsx
@@ -6,6 +6,7 @@ import {
 	getLine0ForEditorLine
 } from "@codestream/webview/store/editorContext/reducer";
 import { CodeStreamState } from "@codestream/webview/store";
+import { useRect, useUpdates } from "@codestream/webview/utilities/hooks";
 
 export default function ContainerAtEditorLine(props: {
 	lineNumber: number;
@@ -20,10 +21,68 @@ export default function ContainerAtEditorLine(props: {
 		};
 	});
 
-	const top = props.lineNumber > 0 ? (window.innerHeight * line0) / visibleLineCount : -1000;
+	const logicalPosition = React.useMemo(
+		() => (props.lineNumber > 0 ? (window.innerHeight * line0) / visibleLineCount : -1000),
+		[props.lineNumber, line0, visibleLineCount]
+	);
+
+	const rootRef = React.useRef<HTMLElement>(null);
+	const rootDimensions = useRect(rootRef, [logicalPosition]);
+
+	const [position, setPosition] = React.useState(logicalPosition);
+	const [adjustedPosition, setAdjustedPosition] = React.useState<number | undefined>(undefined);
+
+	const getAdjustedPosition = React.useCallback((currentPosition: number, rootBottom: number) => {
+		const domBodyDimensions = document.body.getBoundingClientRect();
+		if (rootBottom >= domBodyDimensions.bottom - 15) {
+			const newPosition = currentPosition - (rootBottom - domBodyDimensions.bottom + 120);
+			if (newPosition !== currentPosition) {
+				return newPosition;
+			}
+		}
+
+		return currentPosition;
+	}, []);
+
+	// reposition after mounted in DOM
+	React.useLayoutEffect(() => {
+		let rootDimensions = rootRef.current!.getBoundingClientRect();
+
+		const domBodyDimensions = document.body.getBoundingClientRect();
+		if (rootDimensions.bottom >= domBodyDimensions.bottom) {
+			const nextPosition = getAdjustedPosition(position, rootDimensions.bottom);
+			if (nextPosition !== position) return setAdjustedPosition(nextPosition);
+		}
+	}, []);
+
+	// check whether adjustment is necessary after updates
+	useUpdates(() => {
+		if (adjustedPosition) {
+			// if logicalPosition is higher than the adjustedPosition then adjustment isn't necessary anymore
+			if (logicalPosition <= adjustedPosition) {
+				setAdjustedPosition(undefined);
+				setPosition(logicalPosition);
+				return;
+			}
+		}
+		// if still adjusted and logicalPosition has changed, update position and potentially adjustment
+		if (position !== logicalPosition) {
+			setPosition(logicalPosition);
+			const nextPosition = getAdjustedPosition(
+				logicalPosition,
+				logicalPosition + rootDimensions.height
+			);
+			if (nextPosition !== logicalPosition) setAdjustedPosition(nextPosition);
+		}
+	}, [rootDimensions.height, rootDimensions.bottom, position, adjustedPosition, logicalPosition]);
 
 	return (
-		<span className={`plane-container ${props.className || ""}`} style={{ top }} data-top={top}>
+		<span
+			ref={rootRef}
+			className={`plane-container ${props.className || ""}`}
+			style={{ top: adjustedPosition || position }}
+			data-top={adjustedPosition || position}
+		>
 			{props.children}
 		</span>
 	);
--- shared/ui/utilities/hooks.ts
+++ shared/ui/utilities/hooks.ts
@@ -1,6 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState, useCallback, useLayoutEffect } from "react";
 
 type Fn = () => void;
+
+export function useUpdates(callback: Fn, dependencies: any[] = []) {
+	const onMount = useCallback(() => {
+		isMountedRef.current = true;
+	}, []);
+	const isMountedRef = useRef(false);
+	useEffect(isMountedRef.current ? callback : onMount, dependencies);
+}
 
 export function useInterval(callback: Fn, delay = 1000) {
 	const savedCallback = useRef<Fn>(callback);
@@ -42,4 +50,64 @@ export function useRetryingCallback(fn: () => Promise<any>) {
 		} catch (error) {}
 		canRun.current = true;
 	}, 5000);
+}
+
+type RectResult = {
+	bottom: number;
+	height: number;
+	left: number;
+	right: number;
+	top: number;
+	width: number;
+};
+
+function getRect<T extends HTMLElement>(element?: T): RectResult {
+	let rect: RectResult = {
+		bottom: 0,
+		height: 0,
+		left: 0,
+		right: 0,
+		top: 0,
+		width: 0
+	};
+	if (element) rect = element.getBoundingClientRect();
+	return rect;
+}
+
+export function useRect<T extends HTMLElement>(
+	ref: React.RefObject<T>,
+	dependencies: any[] = []
+): RectResult {
+	const [rect, setRect] = useState<RectResult>(
+		ref && ref.current ? getRect(ref.current) : getRect()
+	);
+
+	const handleResize = useCallback(() => {
+		if (!ref.current) return;
+		setRect(getRect(ref.current)); // Update client rect
+	}, [ref]);
+
+	useLayoutEffect(() => {
+		const element = ref.current;
+		if (!element) return;
+
+		handleResize();
+
+		// @ts-ignore
+		if (typeof ResizeObserver === "function") {
+			// @ts-ignore
+			let resizeObserver = new ResizeObserver(() => handleResize());
+			resizeObserver.observe(element);
+			return () => {
+				if (!resizeObserver) return;
+				resizeObserver.disconnect();
+				resizeObserver = null;
+			};
+		} else {
+			window.addEventListener("resize", handleResize); // Browser support, remove freely
+			return () => window.removeEventListener("resize", handleResize);
+		}
+	}, dependencies);
+
+	return rect;
 }
