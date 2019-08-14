c1a53dcf3017ab8d580188dfbdb1561a03d2c02c
Brian Canzanella <brian@codestream.com>
2019-08-14 18:05:56 -0400

    fixes the build!

---
+++ vs/src/CodeStream.VisualStudio.Core/Packages/ToolWindowProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CodeStream.VisualStudio.Core.Models;
+
+namespace CodeStream.VisualStudio.Core.Packages {
+	public interface IToolWindowProvider {
+		bool? ToggleToolWindowVisibility(Guid toolWindowId);
+		bool ShowToolWindowSafe(Guid toolWindowId);
+		bool IsVisible(Guid toolWindowId);
+	}
+
+	public interface SToolWindowProvider { }
+
+	public interface SOptionsDialogPageAccessor { }
+	public interface IOptionsDialogPageAccessor {
+		IOptionsDialogPage GetOptionsDialogPage();
+	}
+
+	public class OptionsDialogPageAccessor : IOptionsDialogPageAccessor, SOptionsDialogPageAccessor {
+		private IOptionsDialogPage _optionsDialogPage;
+		public OptionsDialogPageAccessor(IOptionsDialogPage optionsDialogPage) {
+			_optionsDialogPage = optionsDialogPage;
+		}
+		public IOptionsDialogPage GetOptionsDialogPage() {
+			return _optionsDialogPage;
+		}
+	}
+}
