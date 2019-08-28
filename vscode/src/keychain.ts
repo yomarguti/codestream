5a636b4d21b7d05d1acd13df551431123785dcf1
Eric Amodio <eric@codestream.com>
2019-08-28 14:35:31 -0400

    Updates keytar loading

--- vscode/src/keychain.ts
+++ vscode/src/keychain.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 
-// Copied from https://github.com/Microsoft/vscode-pull-request-github/blob/master/src/common/keychain.ts
+// Copied from https://github.com/microsoft/vscode-pull-request-github/blob/df0b143714b6a17d74f8e22dddb0863d7e71fdb8/src/authentication/keychain.ts
 
 // keytar depends on a native module shipped in vscode, so this is
 // how we load it
@@ -10,15 +10,8 @@ function getNodeModule<T>(moduleName: string): T | undefined {
 	// tslint:disable-next-line:no-eval
 	const vscodeRequire = eval("require");
 	try {
-		return vscodeRequire(`${vscode.env.appRoot}/node_modules.asar/${moduleName}`);
-	} catch (ex) {
-		// Not in ASAR.
-	}
-	try {
-		return vscodeRequire(`${vscode.env.appRoot}/node_modules/${moduleName}`);
-	} catch (ex) {
-		// Not available.
-	}
+		return vscodeRequire(moduleName);
+	} catch (ex) {}
 	return undefined;
 }
 
