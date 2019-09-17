419c5c1eab3cded220142502ccf71703124b37a9
akonwi <akonwi@codestream.com>
2019-09-17 16:50:43 -0400

    update scripts used for releasing

--- atom/bin/clean-metadata.js
+++ atom/bin/clean-metadata.js
@@ -2,10 +2,12 @@
 const fs = require("fs");
 
 const fileName = process.argv[2];
+const previousVersion = process.argv[3];
 
 const packageMetadata = JSON.parse(fs.readFileSync(fileName, "utf8"));
 packageMetadata.dependencies = {};
 packageMetadata.devDependencies = {};
 packageMetadata.scripts = {};
+packageMetadata.version = previousVersion;
 
 fs.writeFileSync(fileName, JSON.stringify(packageMetadata, undefined, "\t"), "utf8");
