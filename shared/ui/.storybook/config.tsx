b6bbbfa723dd9435dd1231e4d000aa900c50a477
akonwi <akonwi@codestream.com>
2019-11-04 10:56:06 -0500

    add dark mode toggle in storybook

--- shared/ui/.storybook/config.tsx
+++ shared/ui/.storybook/config.tsx
@@ -1,8 +1,20 @@
 import * as React from "react";
+import addons from "@storybook/addons";
 import { configure, addDecorator } from "@storybook/react";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "../src/themes";
 
 configure(require.context("../src", true, /\.stories\.tsx$/), module);
 
-addDecorator(story => <ThemeProvider theme={lightTheme}>{story()}</ThemeProvider>);
+const channel = addons.getChannel();
+
+addDecorator(story => {
+	const [isDark, setIsDark] = React.useState(false);
+
+	React.useEffect(() => {
+		channel.on("DARK_MODE", setIsDark);
+		return () => (channel as any).off("DARK_MODE", setIsDark);
+	}, [channel]);
+
+	return <ThemeProvider theme={isDark ? darkTheme : lightTheme}>{story()}</ThemeProvider>;
+});
