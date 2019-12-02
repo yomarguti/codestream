10d7af5bd27d93241ff476d0e6d9ca5266375847
akonwi <akonwi@codestream.com>
2019-12-02 16:19:59 -0500

    Add babel-plugin-styled-components for webview

--- vscode/webpack.config.js
+++ vscode/webpack.config.js
@@ -255,7 +255,12 @@ function getWebviewConfig(env) {
 				},
 				{
 					test: /\.(js|ts)x?$/,
-					use: "babel-loader",
+					use: {
+						loader: "babel-loader",
+						options: {
+							plugins: ["babel-plugin-styled-components"]
+						}
+					},
 					exclude: /node_modules/
 				},
 				{
