f8f8f742cd0a3ba299e26b4769d74eee10478cb8
James Price <jimmyjayp@gmail.com>
2020-02-08 16:58:02 -0500

    First phase of cleanup following changes to build process to support the unified config file

--- vscode/sandbox/dev.sh
+++
@@ -1,4 +0,0 @@
-
-# Used for the development asset build
-. $VSCSB_TOP/sandbox/defaults.sh
-export VSCSB_ASSET_ENV=dev
--- vscode/sandbox/pi.sh
+++ vscode/sandbox/pi.sh
@@ -1,5 +1,5 @@
 
 # Used for the production asset build
-. $VSCSB_TOP/sandbox/defaults.sh
 export VSCSB_ASSET_ENV=prod
+. $VSCSB_TOP/sandbox/defaults.sh
 unset VSCSB_NOPUBLISH
