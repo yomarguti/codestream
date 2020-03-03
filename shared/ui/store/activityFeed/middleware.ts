117c8f616ba1ad2662e439da9c8dcddece36f5a0
akonwi <akonwi@gmail.com>
2020-03-03 12:34:45 -0500

    Revert "Don't create activity entries for change request codemarks"
    This reverts commit 4efdf81d63c57e29eaf8751de1a989694e88d760.

--- shared/ui/store/activityFeed/middleware.ts
+++ shared/ui/store/activityFeed/middleware.ts
@@ -24,10 +24,7 @@ export const activityFeedMiddleware = (
 				if (post.parentPostId != null) {
 					// ensure we have the parent post
 					store.dispatch(fetchPostForActivity(post.parentPostId, post.streamId));
-				} else if (
-					post.codemark != null &&
-					(post.codemark.reviewId == null || post.codemark.isChangeRequest)
-				) {
+				} else if (post.codemark != null && post.codemark.reviewId == null) {
 					store.dispatch(addNewActivity("codemark", [post.codemark]));
 				} else if (post.review != null) {
 					store.dispatch(addNewActivity("review", [post.review]));
