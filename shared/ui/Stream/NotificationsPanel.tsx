84b1039171aa11514a102c96eff784c8be364902
Peter Pezaris <pez@codestream.com>
2020-03-06 09:33:16 -0500

    desktop vs. email notification preference

--- shared/ui/Stream/NotificationsPanel.tsx
+++ shared/ui/Stream/NotificationsPanel.tsx
@@ -9,13 +9,15 @@ import { HostApi } from "../webview-api";
 export const NotificationsPanel = props => {
 	const dispatch = useDispatch();
 	const derivedState = useSelector((state: CodeStreamState) => {
+		const hasDesktopNotifications = state.ide.name === "VSC" || state.ide.name === "JETBRAINS";
 		return {
 			notificationPreference: state.preferences.notifications || "involveMe",
-			isInVscode: state.ide.name === "VSC",
-			isInJetbrains: state.ide.name === "JETBRAINS"
+			notificationDeliveryPreference: state.preferences.notificationDelivery || "both",
+			hasDesktopNotifications
 		};
 	});
 	const [loading, setLoading] = useState(false);
+	const [loadingDelivery, setLoadingDelivery] = useState(false);
 
 	const handleChange = async (value: string) => {
 		setLoading(true);
@@ -23,6 +25,14 @@ export const NotificationsPanel = props => {
 		// @ts-ignore
 		await dispatch(setUserPreference(["notifications"], value));
 		setLoading(false);
+	};
+
+	const handleChangeDelivery = async (value: string) => {
+		setLoadingDelivery(true);
+		HostApi.instance.track("Notification Delivery Preference Changed", { Value: value });
+		// @ts-ignore
+		await dispatch(setUserPreference(["notificationDelivery"], value));
+		setLoadingDelivery(false);
 	};
 
 	return (
@@ -34,7 +44,7 @@ export const NotificationsPanel = props => {
 				</div>
 				<fieldset className="form-body">
 					<p className="explainer">
-						{derivedState.isInVscode || derivedState.isInJetbrains
+						{derivedState.hasDesktopNotifications
 							? "Follow codemarks and reviews to receive desktop and email notifications."
 							: "Follow codemarks and reviews to receive email notifications."}
 					</p>
@@ -47,10 +57,27 @@ export const NotificationsPanel = props => {
 						>
 							<Radio value="all">Automatically follow all new codemarks and reviews</Radio>
 							<Radio value="involveMe">
-								Follow codemarks and reviews I have created, I have been mentioned in, or I have replied to
+								Follow codemarks and reviews I have created, I have been mentioned in, or I have
+								replied to
 							</Radio>
 							<Radio value="off">Don't automatically follow any codemarks or reviews</Radio>
 						</RadioGroup>
+						{derivedState.hasDesktopNotifications && (
+							<div style={{ marginTop: "20px" }}>
+								<p className="explainer">Deliver notifications via:</p>
+								<RadioGroup
+									name="delivery"
+									selectedValue={derivedState.notificationDeliveryPreference}
+									onChange={handleChangeDelivery}
+									loading={loadingDelivery}
+								>
+									<Radio value="both">Email &amp; Desktop</Radio>
+									<Radio value="email">Email only</Radio>
+									<Radio value="toast">Desktop only</Radio>
+									<Radio value="none">None</Radio>
+								</RadioGroup>
+							</div>
+						)}
 						<p>&nbsp;</p>
 
 						<p>
