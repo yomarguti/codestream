11d0a417ae00fa645cc5b5b4fd3be38b6870bcfe
Marcelo Bukowski de Farias <marcelo@codestream.com>
2020-04-24 12:47:48 -0400

    Upgrade to JxBrowser 7.6

--- jb/src/main/kotlin/webview/BrowserEngineService.kt
+++ jb/src/main/kotlin/webview/BrowserEngineService.kt
@@ -25,7 +25,7 @@ class BrowserEngineService : Disposable {
         // System.setProperty("jxbrowser.ipc.external", "true")
         val optionsBuilder = EngineOptions
             .newBuilder(RenderingMode.OFF_SCREEN)
-            .licenseKey("6P835FT5H9Q596QEMRZTTB0RYSG7H1P664XU2Y2M9QPOA998OY42K6N3OTUO90SSP5BA")
+            .licenseKey("***REMOVED***")
             .userDataDir(Paths.get(dir.toURI()))
             .disableGpu()
             // .disableWebSecurity()
