f19d2ce20e584197cbd13a79d3854313f817cce5
Colin Stryker <colin@codestream.com>
2020-05-06 18:19:31 -0700

    add files

---
+++ shared/agent/src/protocol/agent.protocol.okta.ts
@@ -0,0 +1,5 @@
+"use strict";
+
+export interface OktaConfigurationData {
+	orgId: string;
+}
---
+++ shared/agent/src/providers/okta.ts
@@ -0,0 +1,34 @@
+"use strict";
+import {
+	OktaConfigurationData
+} from "../protocol/agent.protocol";
+import { CSOktaProviderInfo } from "../protocol/api.protocol";
+import { log, lspProvider } from "../system";
+import { ThirdPartyPostProviderBase } from "./provider";
+
+@lspProvider("okta")
+export class OktaProvider extends ThirdPartyPostProviderBase<CSOktaProviderInfo> {
+	get displayName() {
+		return "Okta";
+	}
+
+	get name() {
+		return "okta";
+	}
+
+	get headers() {
+		return {
+			Authorization: `Bearer ${this.accessToken}`
+		};
+	}
+
+	@log()
+	async configure(request: OktaConfigurationData) {
+		await this.session.api.setThirdPartyProviderInfo({
+			providerId: this.providerConfig.id,
+			data: {
+				orgId: request.orgId
+			}
+		});
+	}
+}
