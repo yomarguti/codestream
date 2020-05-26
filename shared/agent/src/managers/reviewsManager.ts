be777e2a29811be3b95879eec655ef384f120ed8
Brian Canzanella <brian@codestream.com>
2020-05-26 13:13:43 -0400

    Telemetry for checkpoints

--- shared/agent/src/managers/reviewsManager.ts
+++ shared/agent/src/managers/reviewsManager.ts
@@ -482,8 +482,9 @@ export class ReviewsManager extends CachedEntityManagerBase<CSReview> {
 		process.nextTick(() => {
 			try {
 				const telemetry = Container.instance().telemetry;
+				// get the highest number checkpoint by sorting by checkpoint descending
 				const totalCheckpoints =
-					reviewChangesets.map(_ => _!.checkpoint || 0).sort((a, b) => (b || 0) - (a || 0))[0] + 1;
+					reviewChangesets.map(_ => _!.checkpoint || 0).sort((a, b) => (b || 0) - (a || 0))[0];
 				const reviewProperties: {
 					[key: string]: any;
 				} = {
