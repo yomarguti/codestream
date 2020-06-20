49ba3e921f8edd357c88625e698b7b47d7a650db
Brian Canzanella <brian@codestream.com>
2020-06-20 16:14:41 -0400

    VS build

--- vs/src/CodeStream.VisualStudio/Pre-Build.ps1
+++ vs/src/CodeStream.VisualStudio/Pre-Build.ps1
@@ -16,30 +16,37 @@ Write-Host "TargetDir=$($TargetDir)"
 $Joined = Join-Path "$($SolutionDir)" "..\licenses\$($ConfigurationName)"
 Write-Host $Joined
 $LicenseFile = (($Joined | Resolve-Path).Path) + "\teamdev.licenses"
-Write-Host "LicenseFile=$($LicenseFile)"
-
+if ((Test-Path -Path "$($LicenseFile)")) {
+	Write-Host "LicenseFile=$($LicenseFile) (exists)"
+}
+else {
+	Write-Host "LicenseFile=$($LicenseFile) (does not exist!!!)"
+}
 
 $LocalLicenseFile = "$($ProjectDir)teamdev.licenses"
 Write-Host "LocalLicenseFile=$($LocalLicenseFile)"
 Write-Host ""
 Write-Host ""
 
-Write-Host "Possible deleting old local license file '$($LocalLicenseFile)'..."
-if ((Test-Path -Path $LocalLicenseFile)) {	
-	Remove-Item $LocalLicenseFile -Force
+Write-Host "Possibly deleting old local license file '$($LocalLicenseFile)'..."
+if ((Test-Path -Path "$($LocalLicenseFile)")) {	
+	Remove-Item "$($LocalLicenseFile)" -Force
 	Write-Host "Deleted old local license completed."
 }
 else {
 	Write-Host "NOT deleting old local license (doesn't exist) '$($LocalLicenseFile)'..."
 }
+Write-Host ""
 
 Write-Host "Copying license to local license..."
-if (!(Test-Path -Path $LicenseFile)) {
+if (!(Test-Path -Path "$($LicenseFile)")) {
  	Write-Host "Actual LicenseFile not found LicenseFile=$($LicenseFile)"
  	exit 1
 }
-Copy-Item $LicenseFile -Destination $ProjectDir
+Copy-Item "$($LicenseFile)" -Destination "$($ProjectDir)"
 Write-Host "Copying licenses completed."
+Write-Host ""
+
 
 Write-Host ""
 Write-Host ""
