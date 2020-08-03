d0e04d21038796c3e9c6f03ca194288417187237
Marcelo Bukowski de Farias <marcelo@codestream.com>
2020-08-03 18:07:11 -0400

    Open links in external browser

--- jb/src/main/kotlin/webview/JBCefWebView.kt
+++ jb/src/main/kotlin/webview/JBCefWebView.kt
@@ -1,7 +1,9 @@
 package com.codestream.webview
 
 import com.codestream.DEBUG
+import com.codestream.system.SPACE_ENCODED
 import com.google.gson.JsonElement
+import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.ui.jcef.JBCefBrowser
 import com.intellij.ui.jcef.JBCefJSQuery
@@ -11,10 +13,13 @@ import org.cef.callback.CefContextMenuParams
 import org.cef.callback.CefMenuModel
 import org.cef.handler.CefContextMenuHandlerAdapter
 import org.cef.handler.CefLoadHandlerAdapter
+import org.cef.handler.CefRequestHandlerAdapter
+import org.cef.network.CefRequest
 
 class JBCefWebView(val jbCefBrowser: JBCefBrowser, val router: WebViewRouter) : WebView {
 
     private val logger = Logger.getInstance(JBCefWebView::class.java)
+    private var loadedUrl: String? = null
 
     val routerQuery: JBCefJSQuery = JBCefJSQuery.create(jbCefBrowser).also {
         it.addHandler { message: String ->
@@ -72,9 +77,29 @@ class JBCefWebView(val jbCefBrowser: JBCefBrowser, val router: WebViewRouter) : 
                 logger.info("Router connected")
             }
         }, jbCefBrowser.cefBrowser)
+
+        jbCefBrowser.jbCefClient.addRequestHandler(object : CefRequestHandlerAdapter() {
+            override fun onBeforeBrowse(
+                browser: CefBrowser?,
+                frame: CefFrame?,
+                request: CefRequest?,
+                user_gesture: Boolean,
+                is_redirect: Boolean
+            ): Boolean {
+                return if (request?.url?.startsWith("file://") == true) {
+                    super.onBeforeBrowse(browser, frame, request, user_gesture, is_redirect)
+                } else {
+                    request?.url?.let {
+                        BrowserUtil.browse(it.replace(" ", SPACE_ENCODED))
+                    }
+                    true
+                }
+            }
+        }, jbCefBrowser.cefBrowser)
     }
 
     override fun loadUrl(url: String) {
+        loadedUrl = url
         jbCefBrowser.loadURL(url)
     }
 
