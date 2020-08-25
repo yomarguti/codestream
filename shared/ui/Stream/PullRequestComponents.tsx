2ac8a04e268745129fc6b816d0797557f03d6af1
Peter Pezaris <pez@codestream.com>
2020-08-25 02:53:39 -0400

    https://trello.com/c/wmBZ5PA3/4508-bug-problems-with-display-of-code-block

--- shared/ui/Stream/PullRequestComponents.tsx
+++ shared/ui/Stream/PullRequestComponents.tsx
@@ -739,6 +739,12 @@ export const PRIconButton = styled.div`
 	}
 `;
 
+export const PRCodeCommentPatch = styled.div`
+	margin: 10px -15px 15px -15px;
+	border-top: 1px solid var(--base-border-color);
+	border-bottom: 1px solid var(--base-border-color);
+`;
+
 export const PRCodeCommentBody = styled.div`
 	min-height: 30px;
 	margin: 0 0 20px 0;
--- shared/ui/Stream/PullRequestFilesChangedTab.tsx
+++ shared/ui/Stream/PullRequestFilesChangedTab.tsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
-import { isFeatureEnabled } from "../store/apiVersioning/reducer";
-import Icon from "./Icon";
-import Timestamp from "./Timestamp";
-import Tooltip from "./Tooltip";
-import { PRHeadshotName } from "../src/components/HeadshotName";
-import { PRContent, PRSelectorButtons } from "./PullRequestComponents";
+import { PRSelectorButtons } from "./PullRequestComponents";
 import styled from "styled-components";
 import { ExecuteThirdPartyTypedType } from "@codestream/protocols/agent";
 import { useDidMount } from "../utilities/hooks";
@@ -14,11 +9,8 @@ import { CodeStreamState } from "../store";
 import { PullRequestFilesChanged } from "./PullRequestFilesChanged";
 import { FileStatus } from "@codestream/protocols/api";
 import { LoadingMessage } from "../src/components/LoadingMessage";
-import { PRCommitCard } from "./PullRequestCommitsTab";
-import * as Path from "path-browserify";
-import { prettyPrintOne } from "code-prettify";
-import { escapeHtml } from "../utils";
 import { setUserPreference } from "./actions";
+import { PullRequestPatch } from "./PullRequestPatch";
 
 const PRCommitContent = styled.div`
 	margin: 0 20px 20px 40px;
@@ -31,40 +23,10 @@ const PRDiffHunks = styled.div`
 }
 `;
 
-const PRDiffHunk = styled.div`
-	overflow-x: auto;
-	max-width: 100%;
-	font-size: 12px;
+export const PRDiffHunk = styled.div`
 	border: 1px solid var(--base-border-color);
 	border-radius: 5px;
-	overflow: hidden;
 	margin: 0 0 20px 0;
-	pre {
-		display: inline;
-		white-space: pre !important;
-		margin: 0;
-	}
-	pre > div {
-		width: 100%;
-		padding: 3px 10px;
-		// color: #24292e;
-		// background: #fff;
-		> span.linenum {
-			opacity: 0.5;
-		}
-		margin: 0;
-	}
-	.added {
-		background: #e6ffed;
-		background: rgba(80, 255, 0, 0.1);
-	}
-	.deleted {
-		background: #ffeef0;
-		background: rgba(255, 0, 0, 0.12);
-	}
-	.header {
-		background: rgba(0, 150, 255, 0.1);
-	}
 	h1 {
 		font-size: 12px;
 		font-weight: normal;
@@ -130,74 +92,6 @@ export const PullRequestFilesChangedTab = props => {
 
 	if (!filesChanged || !filesChanged.length) return null;
 
-	const renderPatch = (patch: string, filename: string) => {
-		if (!patch) return null;
-		let leftLine;
-		let rightLine;
-		let width;
-
-		let extension = Path.extname(filename).toLowerCase();
-		if (extension.startsWith(".")) {
-			extension = extension.substring(1);
-		}
-
-		const renderLineNum = line => (
-			<span className="linenum">{(line + "").padStart(width, " ") + "  "}</span>
-		);
-
-		const syntaxHighlight = string => {
-			const string2 = string.slice(0, 1) + " " + string.slice(1);
-			const html = prettyPrintOne(escapeHtml(string2), extension);
-			return <pre className="prettyprint" dangerouslySetInnerHTML={{ __html: html }} />;
-		};
-
-		return patch.split("\n").map(_ => {
-			if (_.indexOf("@@ ") === 0) {
-				const matches = _.match(/@@ \-(\d+).*? \+(\d+)/);
-				if (matches) {
-					leftLine = matches[1];
-					rightLine = matches[2];
-					width = Math.max(5, rightLine.length + 1);
-				}
-				return (
-					<div className="header">
-						{renderLineNum("")}
-						{renderLineNum("")}
-						<pre className="prettyprint">{_}</pre>
-					</div>
-				);
-			} else if (_.indexOf("+") === 0) {
-				rightLine++;
-				return (
-					<div className="added">
-						{renderLineNum("")}
-						{renderLineNum(rightLine)}
-						{syntaxHighlight(_)}
-					</div>
-				);
-			} else if (_.indexOf("-") === 0) {
-				leftLine++;
-				return (
-					<div className="deleted">
-						{renderLineNum(leftLine)}
-						{renderLineNum("")}
-						{syntaxHighlight(_)}
-					</div>
-				);
-			} else {
-				leftLine++;
-				rightLine++;
-				return (
-					<div>
-						{renderLineNum(leftLine)}
-						{renderLineNum(rightLine)}
-						{syntaxHighlight(_)}
-					</div>
-				);
-			}
-		});
-	};
-
 	const mode = derivedState.pullRequestFilesChangedMode;
 
 	return (
@@ -216,10 +110,11 @@ export const PullRequestFilesChangedTab = props => {
 			) : (
 				<PRDiffHunks>
 					{filesChanged.map(_ => {
+						console.warn("PATCH IS: ", _.patch);
 						return (
 							<PRDiffHunk>
 								<h1>{_.filename}</h1>
-								<pre>{renderPatch(_.patch, _.filename)}</pre>
+								<PullRequestPatch patch={_.patch} filename={_.filename} />
 							</PRDiffHunk>
 						);
 					})}
