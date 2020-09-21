cbe4b899f0490ad6477bf7604c16f8f4db6871cb
Brian Canzanella <brian@codestream.com>
2020-09-21 18:47:12 -0400

    PRs aren't appearing because it doesn't think I have repo open https://trello.com/c/VJpPbG55/4746-prs-arent-appearing-because-it-doesnt-think-i-have-repo-open

--- shared/agent/src/providers/registry.ts
+++ shared/agent/src/providers/registry.ts
@@ -541,7 +541,6 @@ export class ThirdPartyProviderRegistry {
 	async queryThirdParty(request: QueryThirdPartyRequest) {
 		try {
 			if (!request || !request.url) return undefined;
-			if (!request.url.toLowerCase().startsWith("http")) return undefined;
 
 			const uri = URI.parse(request.url);
 			const providers = getRegisteredProviders();
