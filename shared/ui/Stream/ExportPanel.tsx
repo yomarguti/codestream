a0826f664dd28b4f4e88afaee578de47b33a2eda
Peter Pezaris <pez@codestream.com>
2020-09-22 15:46:31 -0400

    "Data export light"

--- shared/ui/Stream/ExportPanel.tsx
+++ shared/ui/Stream/ExportPanel.tsx
@@ -10,6 +10,8 @@ import { CodemarkType } from "@codestream/protocols/api";
 import { PanelHeader } from "../src/components/PanelHeader";
 import { CreateCodemarkIcons } from "./CreateCodemarkIcons";
 import { createSelector } from "reselect";
+import CancelButton from "./CancelButton";
+import { closePanel } from "./actions";
 
 const getSearchableCodemarks = createSelector(
 	(state: CodeStreamState) => state.codemarks,
@@ -31,7 +33,7 @@ export const ExportPanel = () => {
 	const derivedState = useSelector((state: CodeStreamState) => {
 		const codemarks = useSelector(getSearchableCodemarks);
 
-		return { codemarks, webviewFocused: state.context.hasFocus };
+		return { codemarks, webviewFocused: state.context.hasFocus, repos: state.repos };
 	});
 
 	const [text, setText] = React.useState("");
@@ -43,22 +45,48 @@ export const ExportPanel = () => {
 
 	const insertText = () => {};
 
+	const escapeText = text => {
+		const returnText = text.replace(/\n/g, "\\\n");
+		if (returnText.match(/\"/)) return `"${returnText.replace(/\"/g, '\\"')}"`;
+		else return returnText;
+	};
 	return (
 		<div className="panel full-height activity-panel">
 			<CreateCodemarkIcons />
-			<PanelHeader title="Export"></PanelHeader>
+			<PanelHeader title="Data Export">
+				<CancelButton onClick={() => dispatch(closePanel())} />
+			</PanelHeader>
 			<ScrollBox>
 				<div className="channel-list vscroll">
-					<pre className="monospace" style={{ margin: "20px" }}>
+					<pre
+						className="monospace"
+						style={{ margin: "20px", whiteSpace: "nowrap", overflow: "auto" }}
+					>
 						// repo,file,commitSha,location,date,author,id,parentId,type,title,body,assignees
 						<br />
 						{derivedState.codemarks.map(codemark => {
-							return (
-								<>
-									{codemark.text || codemark.title}
-									<br />
-								</>
-							);
+							if (codemark.markers) {
+								return codemark.markers.map(marker => {
+									const location = marker.referenceLocations[marker.referenceLocations.length - 1];
+									const repo = derivedState.repos[marker.repoId];
+									const repoName = repo ? repo.name : "";
+									return (
+										<>
+											{repoName},{marker.file},{location.commitHash},{location.location[0]},
+											{codemark.createdAt},{codemark.creatorId},{codemark.id},{codemark.type},
+											{escapeText(codemark.title || codemark.text)},
+											{escapeText(codemark.title ? codemark.text : "")}
+											<br />
+										</>
+									);
+								});
+							} else
+								return (
+									<>
+										{escapeText(codemark.text || codemark.title)}
+										<br />
+									</>
+								);
 						})}
 					</pre>
 				</div>
