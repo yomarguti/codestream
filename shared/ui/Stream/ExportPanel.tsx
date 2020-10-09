0d818d9a2dc647cabd1628f7fcaec5b320075c6c
Peter Pezaris <pez@codestream.com>
2020-10-09 02:45:45 -0400

    https://trello.com/c/hfyVik4t/4905-cant-select-output-from-data-export and https://trello.com/c/g46Fm6CD/4915-data-export-triggers-unexpected-error-in-prod

--- shared/ui/Stream/ExportPanel.tsx
+++ shared/ui/Stream/ExportPanel.tsx
@@ -12,6 +12,8 @@ import { CreateCodemarkIcons } from "./CreateCodemarkIcons";
 import { createSelector } from "reselect";
 import CancelButton from "./CancelButton";
 import { closePanel } from "./actions";
+import Icon from "./Icon";
+import copy from "copy-to-clipboard";
 
 const getSearchableCodemarks = createSelector(
 	(state: CodeStreamState) => state.codemarks,
@@ -46,49 +48,65 @@ export const ExportPanel = () => {
 	const insertText = () => {};
 
 	const escapeText = text => {
-		const returnText = text.replace(/\n/g, "\\\n");
+		const returnText = text.replace(/\n/g, "\\" + "n");
 		if (returnText.match(/\"/)) return `"${returnText.replace(/\"/g, '\\"')}"`;
 		else return returnText;
 	};
+
+	let output =
+		"// repo,file,commitSha,location,date,author,id,parentId,type,title,body,assignees\n";
+	derivedState.codemarks.forEach(codemark => {
+		if (!codemark) return;
+		if (codemark.markers) {
+			codemark.markers.map(marker => {
+				if (!marker) return;
+				const location: any = marker.referenceLocations
+					? marker.referenceLocations[marker.referenceLocations.length - 1] || {}
+					: {};
+				const repo = derivedState.repos[marker.repoId];
+				const repoName = repo ? repo.name : "";
+				output += `${repoName},${marker.file},${location.commitHash},${
+					location.location ? location.location[0] : ""
+				},${codemark.createdAt},${codemark.creatorId},${codemark.id},${codemark.type},${escapeText(
+					codemark.title || codemark.text
+				)},${escapeText(codemark.title ? codemark.text : "")}\n`;
+			});
+		} else {
+			output += `${escapeText(codemark.text || codemark.title)}\n`;
+		}
+	});
+
 	return (
 		<div className="panel full-height activity-panel">
 			<CreateCodemarkIcons />
-			<PanelHeader title="Data Export">
+			<PanelHeader
+				title={
+					<span>
+						Data Export{" "}
+						<Icon
+							name="copy"
+							className="clickable"
+							onClick={() => copy(output)}
+							title="Copy Export to Clipboard"
+						/>
+					</span>
+				}
+			>
 				<CancelButton onClick={() => dispatch(closePanel())} />
 			</PanelHeader>
 			<ScrollBox>
-				<div className="channel-list vscroll">
-					<pre
+				<div className="channel-list vscroll" style={{ padding: "0 0 0 0" }}>
+					<textarea
 						className="monospace"
-						style={{ margin: "20px", whiteSpace: "nowrap", overflow: "auto" }}
+						style={{
+							width: "100%",
+							height: "calc(100% - 5px)",
+							whiteSpace: "nowrap",
+							overflow: "auto"
+						}}
 					>
-						// repo,file,commitSha,location,date,author,id,parentId,type,title,body,assignees
-						<br />
-						{derivedState.codemarks.map(codemark => {
-							if (codemark.markers) {
-								return codemark.markers.map(marker => {
-									const location = marker.referenceLocations[marker.referenceLocations.length - 1];
-									const repo = derivedState.repos[marker.repoId];
-									const repoName = repo ? repo.name : "";
-									return (
-										<>
-											{repoName},{marker.file},{location.commitHash},{location.location[0]},
-											{codemark.createdAt},{codemark.creatorId},{codemark.id},{codemark.type},
-											{escapeText(codemark.title || codemark.text)},
-											{escapeText(codemark.title ? codemark.text : "")}
-											<br />
-										</>
-									);
-								});
-							} else
-								return (
-									<>
-										{escapeText(codemark.text || codemark.title)}
-										<br />
-									</>
-								);
-						})}
-					</pre>
+						{output}
+					</textarea>
 				</div>
 			</ScrollBox>
 		</div>
