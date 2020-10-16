34b5998b72cae9a5d0ab4361a62a8dea49545015
Marcelo Bukowski de Farias <marcelo@codestream.com>
2020-10-16 18:23:59 -0400

    Create tool window content to get our title properly displayed

--- jb/src/main/kotlin/webview/CodeStreamToolWindowFactory.kt
+++ jb/src/main/kotlin/webview/CodeStreamToolWindowFactory.kt
@@ -4,9 +4,17 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import java.awt.BorderLayout
+import javax.swing.JPanel
 
 class CodeStreamToolWindowFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        toolWindow.title = "CodeStream"
+        toolWindow.contentManager.addContent(
+            toolWindow.contentManager.factory.createContent(
+                JPanel(BorderLayout()),
+                "",
+                false
+            )
+        )
     }
 }
