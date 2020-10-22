675c1088d4f131693828b2da4382aaa2584d1133
Marcelo Bukowski de Farias <marcelo@codestream.com>
2020-10-22 15:44:16 -0400

    Translate email confirmation to Spanish

--- shared/ui/Authentication/EmailConfirmation.tsx
+++ shared/ui/Authentication/EmailConfirmation.tsx
@@ -110,7 +110,9 @@ export const EmailConfirmation = (connect() as any)((props: Props) => {
 			<form className="standard-form" onSubmit={onSubmit}>
 				<fieldset className="form-body">
 					<div className="border-bottom-box">
-						<h3>Check Your Email</h3>
+						<h3>
+							<FormattedMessage id="confirmation.checkEmail" defaultMessage="Check Your Email" />
+						</h3>
 						<FormattedMessage id="confirmation.instructions" tagName="p" />
 						<FormattedMessage id="confirmation.didNotReceive">
 							{text => (
