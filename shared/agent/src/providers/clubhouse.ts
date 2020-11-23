193981255b2d9f6352e7c4cfd8822a52a459b79e
Colin Stryker <colin@codestream.com>
2020-11-22 20:23:33 -0800

    add files

---
+++ shared/agent/src/providers/clubhouse.ts
@@ -0,0 +1,131 @@
+"use strict";
+import { url } from "inspector";
+import * as qs from "querystring";
+import { Logger } from "../logger";
+import {
+	ClubhouseConfigurationData,
+	ClubhouseCreateCardRequest,
+	ClubhouseCreateCardResponse,
+	ClubhouseMember,
+	ClubhouseProject,
+	ClubhouseSelf,
+	ClubhouseStory,
+	CreateThirdPartyCardRequest,
+	FetchThirdPartyBoardsRequest,
+	FetchThirdPartyBoardsResponse,
+	FetchThirdPartyCardsRequest,
+	FetchThirdPartyCardsResponse,
+	MoveThirdPartyCardRequest,
+	ThirdPartyProviderCard
+} from "../protocol/agent.protocol";
+import { CSClubhouseProviderInfo } from "../protocol/api.protocol";
+import { log, lspProvider } from "../system";
+import { ApiResponse, ThirdPartyIssueProviderBase } from "./provider";
+
+@lspProvider("clubhouse")
+export class ClubhouseProvider extends ThirdPartyIssueProviderBase<CSClubhouseProviderInfo> {
+	private _clubhouseUserInfo: ClubhouseSelf | undefined;
+
+	get displayName() {
+		return "Clubhouse";
+	}
+
+	get name() {
+		return "clubhouse";
+	}
+
+	get headers() {
+		return {
+			"Content-Type": "application/json",
+			"Clubhouse-Token": this.accessToken!
+		};
+	}
+
+	@log()
+	async configure(request: ClubhouseConfigurationData) {
+		await this.session.api.setThirdPartyProviderToken({
+			providerId: this.providerConfig.id,
+			token: request.token
+		});
+	}
+
+	async onConnected() {
+		this._clubhouseUserInfo = await this.getMemberInfo();
+	}
+
+	@log()
+	async getBoards(request: FetchThirdPartyBoardsRequest): Promise<FetchThirdPartyBoardsResponse> {
+		await this.ensureConnected();
+
+		const response = await this.get<ClubhouseProject[]>("/projects");
+		return { boards: response.body };
+	}
+
+	@log()
+	async getCards(request: FetchThirdPartyCardsRequest): Promise<FetchThirdPartyCardsResponse> {
+		await this.ensureConnected();
+
+		try {
+			if (!request.customFilter) {
+				request.customFilter = `is:story owner:${this._clubhouseUserInfo!.mention_name}`;
+			}
+			const url = `/search?${qs.stringify({ query: request.customFilter })}`;
+			const result = await this.get<any>(url);
+			const stories = result.body.stories.data;
+			const cards: ThirdPartyProviderCard[] = stories.map((story: ClubhouseStory) => {
+				return {
+					id: story.id,
+					url: story.app_url,
+					title: story.name,
+					modifiedAt: new Date(story.updated_at).getTime(),
+					tokenId: story.id,
+					body: story.description
+				};
+			});
+
+			cards.sort((a, b) => {
+				return a.modifiedAt - b.modifiedAt;
+			});
+			return { cards };
+		} catch (e) {
+			Logger.log("Error from Clubhouse: ", JSON.stringify(e, null, 4));
+			return { cards: [] };
+		}
+	}
+
+	@log()
+	async createCard(request: CreateThirdPartyCardRequest) {
+		await this.ensureConnected();
+
+		const data = request.data as ClubhouseCreateCardRequest;
+		const body = {
+			project_id: data.projectId,
+			name: data.name,
+			description: data.description,
+			owner_ids: (data.assignees! || []).map(a => a.id)
+		};
+		const response = await this.post<{}, ClubhouseCreateCardResponse>(
+			`/stories`,
+			body
+		);
+		return { ...response.body, url: response.body.app_url };
+	}
+
+	@log()
+	async moveCard(request: MoveThirdPartyCardRequest) {
+		return { success: false };
+	}
+
+	@log()
+	async getAssignableUsers(request: { boardId: string }) {
+		await this.ensureConnected();
+		const { body } = await this.get<ClubhouseMember[]>("/members");
+		const users = body.filter(u => !u.profile.deactivated);
+		return { users: body.map(u => ({ ...u, displayName: u.profile.name })) };
+	}
+
+	private async getMemberInfo(): Promise<ClubhouseSelf> {
+		const response = await this.get<ClubhouseSelf>("/member");
+		return response.body;
+	}
+}
---
+++ shared/ui/Stream/ConfigureTokenProviderPanel.js
@@ -0,0 +1,148 @@
+import React, { Component } from "react";
+import { injectIntl } from "react-intl";
+import { connect } from "react-redux";
+import { closePanel } from "./actions";
+import { configureProvider } from "../store/providers/actions";
+import CancelButton from "./CancelButton";
+import Button from "./Button";
+import { PROVIDER_MAPPINGS } from "./CrossPostIssueControls/types";
+import { Link } from "../Stream/Link";
+
+export class ConfigureTokenProviderPanel extends Component {
+	initialState = {
+		token: "",
+		tokenTouched: false,
+		formTouched: false
+	};
+
+	state = this.initialState;
+
+	focusInput() {
+		document.getElementById("configure-provider-access-token").focus();
+	}
+
+	componentDidMount() {
+		this.focusInput();
+	}
+
+	componentDidUpdate() {}
+
+	onSubmit = async e => {
+		e.preventDefault();
+		if (this.isFormInvalid()) return;
+		const { providerId } = this.props;
+		const { token } = this.state;
+
+		// configuring is as good as connecting, since we are letting the user
+		// set the access token
+console.warn('CONFIGURING PROVIDER...');
+		this.props.configureProvider(
+			providerId,
+			{ token },
+			true,
+			this.props.originLocation
+		);
+console.warn('CLOSE THE PANEL...');
+		this.props.closePanel();
+console.warn('PANEL CLOSED');
+	};
+
+	renderError = () => {};
+
+	onBlurToken = () => {
+		this.setState({ tokenTouched: true });
+	};
+
+	renderTokenHelp = () => {
+		const { token, tokenTouched, formTouched } = this.state;
+		if (tokenTouched || formTouched) {
+			if (token.length === 0) return <small className="error-message">Required</small>;
+		}
+	};
+
+	tabIndex = () => {};
+
+	isFormInvalid = () => {
+		return this.state.token.length === 0;
+	};
+
+	render() {
+		const { providerId } = this.props;
+		const inactive = false;
+		const { name, scopes } = this.props.providers[providerId] || {};
+		const providerName = PROVIDER_MAPPINGS[name] ? PROVIDER_MAPPINGS[name].displayName : "";
+		const providerShortName = PROVIDER_MAPPINGS[name]
+			? PROVIDER_MAPPINGS[name].shortDisplayName || providerName
+			: "";
+		const helpUrl = PROVIDER_MAPPINGS[name] ? PROVIDER_MAPPINGS[name].helpUrl : "";
+		return (
+			<div className="panel configure-provider-panel">
+				<form className="standard-form vscroll" onSubmit={this.onSubmit}>
+					<div className="panel-header">
+						<CancelButton onClick={this.props.closePanel} />
+						<span className="panel-title">Configure {providerName}</span>
+					</div>
+					<fieldset className="form-body" disabled={inactive}>
+						{this.renderError()}
+						<div id="controls">
+							<div key="token" id="configure-enterprise-controls-token" className="control-group">
+								<label>
+									<strong>{providerShortName} API Token</strong>
+								</label>
+								<label>
+									Please provide an <Link href={helpUrl}>API Token</Link> we can use to access
+									your {providerShortName} projects and issues.
+									{scopes && scopes.length && (
+										<span>
+											&nbsp;Your API Token should have the following scopes: <b>{scopes.join(", ")}</b>.
+										</span>
+									)}
+								</label>
+								<input
+									className="input-text control"
+									type="text"
+									name="token"
+									tabIndex={this.tabIndex()}
+									value={this.state.token}
+									onChange={e => this.setState({ token: e.target.value })}
+									onBlur={this.onBlurToken}
+									required={this.state.tokenTouched || this.state.formTouched}
+									id="configure-provider-access-token"
+								/>
+								{this.renderTokenHelp()}
+							</div>
+							<div className="button-group">
+								<Button
+									id="save-button"
+									className="control-button"
+									tabIndex={this.tabIndex()}
+									type="submit"
+									loading={this.state.loading}
+								>
+									Submit
+								</Button>
+								<Button
+									id="discard-button"
+									className="control-button cancel"
+									tabIndex={this.tabIndex()}
+									type="button"
+									onClick={this.props.closePanel}
+								>
+									Cancel
+								</Button>
+							</div>
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = ({ providers, ide }) => {
+	return { providers };
+};
+
+export default connect(mapStateToProps, { closePanel, configureProvider })(
+	injectIntl(ConfigureTokenProviderPanel)
+);
