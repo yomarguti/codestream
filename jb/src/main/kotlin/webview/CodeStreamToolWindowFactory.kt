02d70147eb67950663e3b14466be0df99cf884aa
Marcelo Bukowski de Farias <marcelo@codestream.com>
2020-12-02 23:06:03 -0500

    Add more logging around agent and tool window initialization

--- jb/src/main/kotlin/webview/CodeStreamToolWindowFactory.kt
+++ jb/src/main/kotlin/webview/CodeStreamToolWindowFactory.kt
@@ -3,6 +3,7 @@ package com.codestream.webview
 import com.codestream.agentService
 import com.codestream.webViewService
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.IconLoader
@@ -13,18 +14,28 @@ import javax.swing.JLabel
 import javax.swing.JPanel
 
 class CodeStreamToolWindowFactory : ToolWindowFactory, DumbAware {
+    private val logger = Logger.getInstance(CodeStreamToolWindowFactory::class.java)
+
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        logger.info("Creating tool window content")
         val csPanel = JPanel(BorderLayout())
         val loadingLabel = JLabel("Loading...", IconLoader.getIcon("/images/codestream.svg"), JLabel.CENTER)
         csPanel.add(loadingLabel)
 
         project.agentService?.onDidStart {
+            logger.info("Scheduling webview attachment to tool window")
             val webViewService = project.webViewService ?: return@onDidStart
             ApplicationManager.getApplication().invokeLater {
-                csPanel.remove(loadingLabel)
-                csPanel.add(webViewService.webView)
+                try {
+                    logger.info("Attaching webview to tool window")
+                    csPanel.remove(loadingLabel)
+                    csPanel.add(webViewService.webView)
+                    logger.info("Webview attached to tool window")
+                } catch (e: Exception) {
+                    logger.error(e)
+                }
             }
-        }
+        } ?: logger.info("Unable to schedule webview attachment - project is disposed")
 
         toolWindow.contentManager.addContent(
             toolWindow.contentManager.factory.createContent(
