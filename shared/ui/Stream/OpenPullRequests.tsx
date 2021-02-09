9a9ae563e0cd72080298caae142a3af2dfadf142
Brian Canzanella <brian@codestream.com>
2021-02-09 09:21:14 -0500

    fixes

--- shared/ui/Stream/OpenPullRequests.tsx
+++ shared/ui/Stream/OpenPullRequests.tsx
@@ -664,19 +664,16 @@ export const OpenPullRequests = React.memo((props: Props) => {
 																		<span>
 																			{pr.title} #{pr.number}
 																		</span>
-																		{pr.labels &&
-																			pr.labels.nodes &&
-																			pr.labels.nodes.length > 0 &&
-																			derivedState.showLabels && (
-																				<span className="cs-tag-container">
-																					{pr.labels.nodes.map((_, index) => (
-																						<Tag
-																							key={index}
-																							tag={{ label: _.name, color: `#${_.color}` }}
-																						/>
-																					))}
-																				</span>
-																			)}
+																		{pr.labels && pr.labels.nodes && pr.labels.nodes.length > 0 && (
+																			<span className="cs-tag-container">
+																				{pr.labels.nodes.map((_, index) => (
+																					<Tag
+																						key={index}
+																						tag={{ label: _.name, color: `#${_.color}` }}
+																					/>
+																				))}
+																			</span>
+																		)}
 																		<span className="subtle">{pr.bodyText || pr.body}</span>
 																	</div>
 																	<div className="icons">
